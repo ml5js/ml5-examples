c2aa3764e2b892dfe77c2669c4cea26cc82f1b43
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-03-11 17:19:11 -0400

    manage the imgs from p5 instead of raw html

--- p5js/05_FastStyleTransfer_Simple/sketch.js
+++ p5js/05_FastStyleTransfer_Simple/sketch.js
@@ -6,27 +6,57 @@ This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young Am
 === */
 
 let inputImg;
-let outputOne;
-let outputTwo;
+let resultImg1;
+let resultImg2;
+let statusMsg;
+let transferBtn;
 
 // Create two Fast Style methods with different pre-trained models
-const fs1 = new ml5.FastStyle('models/wave', modelLoaded1);
-const fs2 = new ml5.FastStyle('models/udnie', modelLoaded2);
+const fs1 = new ml5.FastStyle('models/wave', modelLoaded);
+const fs2 = new ml5.FastStyle('models/udnie', modelLoaded);
 
 function setup() {
   noCanvas();
-  inputImg = select('#inputImg').elt;
-  outputOne = select('#resultImgOne').elt;
-  outputTwo = select('#resultImgTwo').elt;
+  // Status Msg
+  statusMsg = createP('Loading Models...');
+  
+  // Transfer Button
+  transferBtn = createButton('Transfer!');
+  transferBtn.mousePressed(transferImages);
+
+  // Input Image
+  createP('Input Image:');
+  inputImg = createImg('img/patagonia.jpg');
+
+  // Style A
+  createP('Style A: The Great Wave off Kanagawa, 1829 - Katsushika Hokusai');
+  createImg('img/wave.jpg');
+  resultImg1 = createImg('');
+
+  // Style B
+  createP('Style B:Udnie (Young American Girl, The Dance), 1913 - Francis Picabia');
+  createImg('img/udnie.jpg');
+  resultImg2 = createImg('');
+  
 }
 
-// A function to be called when the models has been loaded
-function modelLoaded1() {
-  const result = fs1.transfer(inputImg);
-  outputOne.append(result);
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if(fs1.ready && fs2.ready){
+    statusMsg.html('Ready!')
+  }
 }
 
-function modelLoaded2() {
-  const result = fs2.transfer(inputImg);
-  outputTwo.append(result);
+// Apply the transfer to both images!
+function transferImages() {
+  statusMsg.html('Applying Style Transfer...!');
+
+  var styleA = fs1.transfer(inputImg.elt);
+  resultImg1.elt.src = styleA.src;
+
+  var styleB = fs2.transfer(inputImg.elt);
+  resultImg2.elt.src = styleB.src;
+
+  statusMsg.html('Done!');
 }
