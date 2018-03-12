68f1bce3fbd042883a91d7b621b9a1f46e0b767b
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-03-12 18:10:01 -0400

    Removing all p5js local dependencies and replacing them with CDN. ML5.js needs to be updated in npm after the #97 merge is done, only then we can replace that reference. This commit also changes some DOM manipulation in the StyleTrasnfer examples, besides updating the naming of that class to StyleTransfer

--- p5js/05_FastStyleTransfer_Simple/sketch.js
+++ p5js/05_FastStyleTransfer_Simple/sketch.js
@@ -6,44 +6,30 @@ This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young Am
 === */
 
 let inputImg;
-let resultImg1;
-let resultImg2;
 let statusMsg;
 let transferBtn;
 
 // Create two Fast Style methods with different pre-trained models
-const fs1 = new ml5.FastStyle('models/wave', modelLoaded);
-const fs2 = new ml5.FastStyle('models/udnie', modelLoaded);
+const style1 = new ml5.StyleTransfer('models/wave', modelLoaded);
+const style2 = new ml5.StyleTransfer('models/udnie', modelLoaded);
 
 function setup() {
   noCanvas();
   // Status Msg
-  statusMsg = createP('Loading Models...');
-  
+  statusMsg = select('#statusMsg');
+
+  // Get the input image
+  inputImg = select('#inputImg');
+
   // Transfer Button
-  transferBtn = createButton('Transfer!');
+  transferBtn = select('#transferBtn')
   transferBtn.mousePressed(transferImages);
-
-  // Input Image
-  createP('Input Image:');
-  inputImg = createImg('img/patagonia.jpg');
-
-  // Style A
-  createP('Style A: The Great Wave off Kanagawa, 1829 - Katsushika Hokusai');
-  createImg('img/wave.jpg');
-  resultImg1 = createImg('');
-
-  // Style B
-  createP('Style B:Udnie (Young American Girl, The Dance), 1913 - Francis Picabia');
-  createImg('img/udnie.jpg');
-  resultImg2 = createImg('');
-  
 }
 
 // A function to be called when the models have loaded
 function modelLoaded() {
   // Check if both models are loaded
-  if(fs1.ready && fs2.ready){
+  if(style1.ready && style2.ready){
     statusMsg.html('Ready!')
   }
 }
@@ -52,11 +38,11 @@ function modelLoaded() {
 function transferImages() {
   statusMsg.html('Applying Style Transfer...!');
 
-  var styleA = fs1.transfer(inputImg.elt);
-  resultImg1.elt.src = styleA.src;
+  var styleA = style1.transfer(inputImg.elt);
+  createImg(styleA.src).parent('styleA');
 
-  var styleB = fs2.transfer(inputImg.elt);
-  resultImg2.elt.src = styleB.src;
+  var styleB = style2.transfer(inputImg.elt);
+  createImg(styleB.src).parent('styleB');
 
   statusMsg.html('Done!');
 }
