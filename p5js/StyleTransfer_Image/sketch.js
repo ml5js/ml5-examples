c9e4b56a2d68eaa4dc0c1f182f42cf56f0bbf91c
handav <hannahdavis0220@gmail.com>
2018-04-06 16:42:14 -0400

    changing naming convention so that we don't have to constantly change numbers when we add new examples

---
+++ p5js/StyleTransfer_Image/sketch.js
@@ -0,0 +1,48 @@
+/* ===
+ML5 Example
+Fast_Style_Transfer_Simple
+Fast Style Transfer Simple Example with p5.js
+This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+=== */
+
+let inputImg;
+let statusMsg;
+let transferBtn;
+
+// Create two Fast Style methods with different pre-trained models
+const style1 = new ml5.StyleTransfer('models/wave', modelLoaded);
+const style2 = new ml5.StyleTransfer('models/udnie', modelLoaded);
+
+function setup() {
+  noCanvas();
+  // Status Msg
+  statusMsg = select('#statusMsg');
+
+  // Get the input image
+  inputImg = select('#inputImg');
+
+  // Transfer Button
+  transferBtn = select('#transferBtn')
+  transferBtn.mousePressed(transferImages);
+}
+
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if(style1.ready && style2.ready){
+    statusMsg.html('Ready!')
+  }
+}
+
+// Apply the transfer to both images!
+function transferImages() {
+  statusMsg.html('Applying Style Transfer...!');
+
+  var styleA = style1.transfer(inputImg.elt);
+  createImg(styleA.src).parent('styleA');
+
+  var styleB = style2.transfer(inputImg.elt);
+  createImg(styleB.src).parent('styleB');
+
+  statusMsg.html('Done!');
+}
---
+++ p5js/StyleTransfer_Video/sketch.js
@@ -0,0 +1,57 @@
+/* ===
+ML5 Example
+Fast_Style_Transfer_Mirror
+Fast Style Transfer Mirror Example with p5.js
+This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young American Girl, The Dance)
+=== */
+
+let fastStyle;
+let video;
+let modelReady = false;
+let cameraReady = false;
+let startPredict = false;
+let resultImg;
+
+function setup() {
+  createCanvas(300, 300).parent('canvasContainer');
+  pixelDensity(1);
+  background(0);
+  video = createCapture(VIDEO, cameraLoaded);
+  resultImg = createImg('');
+  resultImg.hide();
+  video.size(200, 200);
+  video.hide();
+  style = new ml5.StyleTransfer('models/udnie', modelLoaded);
+}
+
+function draw(){
+  image(resultImg, 0, 0, 300, 300);
+}
+
+function cameraLoaded() {
+  cameraReady = true;
+}
+
+function modelLoaded() {
+  modelReady = true;
+}
+
+function togglePredicting() {
+  startPredict = !startPredict;
+  if(startPredict){
+    select('#controlBtn').html('Stop');
+    predict();
+  } else {
+    select('#controlBtn').html('Start');
+  }
+
+}
+
+function predict() {
+  if(cameraReady && modelReady && startPredict) {
+    const result = style.transfer(video.elt);
+    resultImg.attribute('src', result.src);
+    image(resultImg, 0, 300, 300);
+    setTimeout(() => predict(), 500);
+  }
+}
