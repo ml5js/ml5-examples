3c350648410accea395b2a770084407e5454064c
shiffman <daniel@shiffman.net>
2018-07-26 15:52:12 -0400

    minor changes to posenet example
    remove extra global variable
    some variables to hold objects to reduce chaining variable stuff
    misc formatting

--- p5js/PoseNet/sketch.js
+++ p5js/PoseNet/sketch.js
@@ -11,7 +11,6 @@ PoseNet example using p5.js
 let video;
 let poseNet;
 let poses = [];
-let skeletons = [];
 
 function setup() {
   createCanvas(640, 480);
@@ -20,14 +19,16 @@ function setup() {
 
   // Create a new poseNet method with a single detection
   poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on('pose', function (results) {
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
     poses = results;
   });
   // Hide the video element, and just show the canvas
   video.hide();
 }
 
-function modelReady(){
+function modelReady() {
   select('#status').html('Model Loaded');
 }
 
@@ -44,9 +45,10 @@ function drawKeypoints()  {
   // Loop through all the poses detected
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      let keypoint = poses[i].pose.keypoints[j];
+      let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
         fill(255, 0, 0);
@@ -61,13 +63,13 @@ function drawKeypoints()  {
 function drawSkeleton() {
   // Loop through all the skeletons detected
   for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
     // For every skeleton, loop through all body connections
-    for (let j = 0; j < poses[i].skeleton.length; j++) {
-      let partA = poses[i].skeleton[j][0];
-      let partB = poses[i].skeleton[j][1];
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
       stroke(255, 0, 0);
       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
     }
   }
 }
-
