20737de49bf1e5bba18eb06b457d176d23346dc6
shiffman <daniel@shiffman.net>
2018-11-01 15:06:47 -0400

    simplifying the sketchRNN basic example

--- p5js/SketchRNN/sketch.js
+++ p5js/SketchRNN/sketch.js
@@ -8,148 +8,50 @@ ml5 Example
 SketchRNN
 === */
 
-var model;
-var dx, dy; // offsets of the pen strokes, in pixels
-var x, y; // absolute coordinates on the screen of where the pen is
-var prevPen = {
-  dx: 0,
-  dy: 0,
-  pen: 'down'
-};
-var modelLoaded = false;
-var runningStep = false;
-var initialStroke = true;
-const options = {
-  temperature: 0.1
-}
-
-// Initial Strokes: this create a circle
-const initialStrokes = [{
-  dx: -4,
-  dy: 0,
-  pen: "down"
-}, {
-  dx: -15,
-  dy: 9,
-  pen: "down"
-}, {
-  dx: -10,
-  dy: 17,
-  pen: "down"
-}, {
-  dx: -1,
-  dy: 28,
-  pen: "down"
-},{
-  dx: 14,
-  dy: 13,
-  pen: "down"
-},{
-  dx: 12,
-  dy: 4,
-  pen: "down"
-},{
-  dx: 22,
-  dy: 1,
-  pen: "down"
-},{
-  dx: 14,
-  dy: -11,
-  pen: "down"
-},{
-  dx: 5,
-  dy: -12,
-  pen: "down"
-},{
-  dx: 2,
-  dy: -19,
-  pen: "down"
-},{
-  dx: -12,
-  dy: -23,
-  pen: "down"
-},{
-  dx: -13,
-  dy: -7,
-  pen: "down"
-},{
-  dx: -14,
-  dy: -1,
-  pen: "up"
-}];
+let model;
+let previous_pen = 'down';
+let x, y;
+let sketch;
 
 function setup() {
-  createCanvas(500, 500);
-  model = new ml5.SketchRNN('windmill', modelReady)
+  createCanvas(400, 300);
+  model = ml5.SketchRNN('cat', modelReady);
+  let button = select('#draw');
+  button.mousePressed(startDrawing);
 }
 
-function modelReady() {
-  select('#status').html('Model Loaded');
-  modelLoaded = true;
-  restart();
+function startDrawing() {
+  x = width / 2;
+  y = height / 2;
+  background(220);
+  model.reset();
+  model.generate(gotSketch);
 }
 
 function draw() {
-  if (modelLoaded) {
-    if (prevPen.pen === "end") {
-      restart();
+  if (sketch) {
+    if (previous_pen == 'down') {
+      stroke(0);
+      strokeWeight(3.0);
+      line(x, y, x + sketch.dx, y + sketch.dy);
     }
-  
-    if (!runningStep) {
-      runningStep = true;
-      model.generate(options, initialStroke ? initialStrokes : [], gotResult);
-      initialStroke = false;
+    x += sketch.dx;
+    y += sketch.dy;
+    previous_pen = sketch.pen;
+
+    if (sketch.pen !== 'end') {
+      sketch = null;
+      model.generate(gotSketch);
     }
-  }
+  } 
 }
 
-function gotResult(err, result) {
-  if (prevPen.pen === "down") {
-    stroke(255, 0, 0)
-    strokeWeight(3.0);
-    line(x, y, x + result.dx, y + result.dy);
-  }
-
-  x += result.dx;
-  y += result.dy;
-  prevPen = result; 
-  runningStep = false;
+function gotSketch(err, s) {
+  sketch = s;
 }
 
-function clearScreen() {
-  background(255, 255, 255, 255);
-  fill(255, 255, 255, 255);
-};
-
-// A function to draw the initial strokes
-function drawStartingStrokes() {
-  for (let i = 0; i < initialStrokes.length; i++) {
-    if (initialStrokes[i].pen === 'end') {
-      break;
-    }
-    // only draw on the paper if the pen is touching the paper
-    if (initialStrokes[i].pen === 'down') {
-      strokeWeight(4.0);
-      line(x, y, x + initialStrokes[i].dx, y + initialStrokes[i].dy); // draw line connecting prev point to current point.
-    }
-    // update the absolute coordinates from the offsets
-    x += initialStrokes[i].dx;
-    y += initialStrokes[i].dy;
-    // update the previous pen's state to the current one we just sampled
-    prev_pen = initialStrokes[i];
-  }
+function modelReady() {
+  console.log('model ready');
+  startDrawing();
 }
 
-function restart() {
-  model.reset();
-  x = width / 2.0;
-  y = height / 3.0;
-  prevPen = {
-    dx: 0,
-    dy: 0,
-    pen: 'down'
-  };
-  initialStroke = true;
-  clearScreen();
-  drawStartingStrokes();
-}
