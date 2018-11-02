03fb92f5630c1b19fc2164d945c077b663c09b7f
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-11-02 11:32:38 -0400

    anonymous functions

--- p5js/FeatureExtractor/FeatureExtractor_Image_Classification/sketch.js
+++ p5js/FeatureExtractor/FeatureExtractor_Image_Classification/sketch.js
@@ -32,8 +32,10 @@ function setup() {
 
 // A function to be called when the model has been loaded
 function modelReady() {
-  select('#modelStatus').html('Base Model (MobileNet) loaded!');
-  classifier.load('./model/model.json', () => console.log('loaded json'));
+  select('#modelStatus').html('Base Model (MobileNet) Loaded!');
+  classifier.load('./model/model.json', function() {
+    select('#modelStatus').html('Custom Model Loaded!');
+  });
 }
 
 // A function to be called when the video has loaded
@@ -91,7 +93,9 @@ function setupButtons() {
   // Load model
   loadBtn = select('#load');
   loadBtn.changed(function() {
-    classifier.load(loadBtn.elt.files, () => console.log('loaded'));
+    classifier.load(loadBtn.elt.files, function(){
+      select('#modelStatus').html('Custom Model Loaded!');
+    });
   });
 }
 
