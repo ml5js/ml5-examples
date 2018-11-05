d77438769a259084526e57093e156f0e08c355e9
Memo Akten <memo@users.noreply.github.com>
2018-11-05 20:37:57 +0000

    Stateful LSTM (#67)
    * first example to test lstm updates
    
    * add runningInference check to LSTM examples to avoid running model simultaneously multiple times
    
    * added basic working Stateful LSTM example
    
    * remove LSTM Text Mixer example
    
    * fix LSTM stateful example if else code style
    
    * LSTM stateful example visualises probability distributions
    
    * adding new model and feaute extractor example
    
    * updates to new api

--- p5js/FeatureExtractor/FeatureExtractor_Image_Classification/sketch.js
+++ p5js/FeatureExtractor/FeatureExtractor_Image_Classification/sketch.js
@@ -43,7 +43,6 @@ function videoReady () {
   select('#videoStatus').html('Video ready!');
 }
 
-
 // Classify the current frame.
 function classify() {
   classifier.classify(gotResults);
--- p5js/LSTM/LSTM_Text/sketch.js
+++ p5js/LSTM/LSTM_Text/sketch.js
@@ -7,20 +7,21 @@
 ml5 Example
 LSTM Generator example with p5.js
 This uses a pre-trained model on a corpus of Virginia Woolf
-For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/lstm
+For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/charRNN
 === */
 
-let lstm;
+let charRNN;
 let textInput;
 let lengthSlider;
 let tempSlider;
 let button;
+let runningInference = false;
 
 function setup() {
   noCanvas();
 
   // Create the LSTM Generator passing it the model directory
-  lstm = ml5.LSTMGenerator('./models/woolf/', modelReady);
+  charRNN = ml5.charRNN('./models/woolf/', modelReady);
 
   // Grab the DOM elements
   textInput = select('#textInput');
@@ -46,33 +47,40 @@ function modelReady() {
 
 // Generate new text
 function generate() {
-  // Update the status log
-  select('#status').html('Generating...');
+  // prevent starting inference if we've already started another instance
+  // TODO: is there better JS way of doing this?
+ if(!runningInference) {
+    runningInference = true;
 
-  // Grab the original text
-  let original = textInput.value();
-  // Make it to lower case
-  let txt = original.toLowerCase();
+    // Update the status log
+    select('#status').html('Generating...');
 
-  // Check if there's something to send
-  if (txt.length > 0) {
-    // This is what the LSTM generator needs
-    // Seed text, temperature, length to outputs
-    // TODO: What are the defaults?
-    let data = {
-      seed: txt,
-      temperature: tempSlider.value(),
-      length: lengthSlider.value()
-    };
+    // Grab the original text
+    let original = textInput.value();
+    // Make it to lower case
+    let txt = original.toLowerCase();
 
-    // Generate text with the lstm
-    lstm.generate(data, gotData);
+    // Check if there's something to send
+    if (txt.length > 0) {
+      // This is what the LSTM generator needs
+      // Seed text, temperature, length to outputs
+      // TODO: What are the defaults?
+      let data = {
+        seed: txt,
+        temperature: tempSlider.value(),
+        length: lengthSlider.value()
+      };
 
-    // When it's done
-    function gotData(err, result) {
-      // Update the status log
-      select('#status').html('Ready!');
-      select('#result').html(txt + result);
+      // Generate text with the charRNN
+      charRNN.generate(data, gotData);
+
+      // When it's done
+      function gotData(err, result) {
+        // Update the status log
+        select('#status').html('Ready!');
+        select('#result').html(txt + result.sample);
+        runningInference = false;
+      }
     }
   }
 }
