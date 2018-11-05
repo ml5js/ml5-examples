c074553bd90f1e6b690347ee5a2274ed3c2e4bfe
Memo Akten <memo@users.noreply.github.com>
2018-11-05 20:37:57 +0000

    Stateful LSTM (#67)
    * first example to test lstm updates
    
    * add runningInference check to LSTM examples to avoid running model simultaneously multiple times
    
    * added basic working Stateful LSTM example
    
    * remove LSTM Text Mixer example
    
    * fix LSTM stateful example if else code style
    
    * LSTM stateful example visualises probability distributions
    
    * adding new model and feaute extractor example
    
    * updates to new api

--- p5js/LSTM/LSTM_Interactive/sketch.js
+++ p5js/LSTM/LSTM_Interactive/sketch.js
@@ -7,19 +7,20 @@
 ML5 Example
 Interactive LSTM Text Generation Example using p5.js
 This uses a pre-trained model on a corpus of Virginia Woolf
-For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/lstm
+For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/charRNN
 === */
 
-let lstm;
+let charRNN;
 let textInput;
 let tempSlider;
 let lengthSlider;
+let runningInference = false;
 
 function setup() {
   noCanvas();
 
   // Create the LSTM Generator passing it the model directory
-  lstm = ml5.LSTMGenerator('models/woolf/', modelReady);
+  charRNN = ml5.charRNN('models/woolf/', modelReady);
 
   // Grab the DOM elements
   textInput = select('#textInput');
@@ -37,39 +38,46 @@ function modelReady() {
 }
 
 function generate() {
-  // Update the status log
-  select('#status').html('Generating...');
+  // prevent starting inference if we've already started another instance
+  // TODO: is there better JS way of doing this?
+ if(!runningInference) {
+   runningInference = true;
 
-  // Update the length and temperature span elements
-  select('#length').html(lengthSlider.value());
-  select('#temperature').html(tempSlider.value());
+    // Update the status log
+    select('#status').html('Generating...');
 
-  // Grab the original text
-  let original = textInput.value();
-  // Make it to lower case
-  let txt = original.toLowerCase();
+    // Update the length and temperature span elements
+    select('#length').html(lengthSlider.value());
+    select('#temperature').html(tempSlider.value());
 
-  // Check if there's something
-  if (txt.length > 0) {
-    // Here is the data for the LSTM generator
-    let data = {
-      seed: txt,
-      temperature: tempSlider.value(),
-      length: lengthSlider.value()
-    };
+    // Grab the original text
+    let original = textInput.value();
+    // Make it to lower case
+    let txt = original.toLowerCase();
 
-    // Generate text with the lstm
-    lstm.generate(data, gotData);
+    // Check if there's something
+    if (txt.length > 0) {
+      // Here is the data for the LSTM generator
+      let data = {
+        seed: txt,
+        temperature: tempSlider.value(),
+        length: lengthSlider.value()
+      };
 
-    // Update the DOM elements with typed and generated text
-    function gotData(err, result) {
-      select('#status').html('Ready!');
-      select('#original').html(original);
-      select('#prediction').html(result);
+      // Generate text with the charRNN
+      charRNN.generate(data, gotData);
+
+      // Update the DOM elements with typed and generated text
+      function gotData(err, result) {
+        select('#status').html('Ready!');
+        select('#original').html(original);
+        select('#prediction').html(result.sample);
+        runningInference = false;
+      }
+    } else {
+      // Clear everything
+      select('#original').html('');
+      select('#prediction').html('');
     }
-  } else {
-    // Clear everything
-    select('#original').html('');
-    select('#prediction').html('');
   }
 }
