782aa6cb5522b7204686d523b5d1469d2749fe49
shiffman <daniel@shiffman.net>
2018-11-08 13:53:45 -0500

    cleanup of basic sketchRNN example

--- p5js/SketchRNN/sketch.js
+++ p5js/SketchRNN/sketch.js
@@ -8,50 +8,68 @@ ml5 Example
 SketchRNN
 === */
 
+// The SketchRNN model
 let model;
+// Start by drawing
 let previous_pen = 'down';
+// Current location of drawing
 let x, y;
-let sketch;
+// The current "stroke" of the drawing
+let strokePath;
 
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(640, 480);
+  background(220);
+  // Load the model
+  // See a list of all supported models: https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
   model = ml5.SketchRNN('cat', modelReady);
+
+  // Button to start drawing
   let button = select('#draw');
   button.mousePressed(startDrawing);
 }
 
+// Reset the drawing
 function startDrawing() {
+  background(220);
+  // Start in the middle
   x = width / 2;
   y = height / 2;
-  background(220);
   model.reset();
-  model.generate(gotSketch);
+  // Generate the first stroke path
+  model.generate(gotStroke);
 }
 
 function draw() {
-  if (sketch) {
+  // If something new to draw
+  if (strokePath) {
+    // If the pen is down, draw a line
     if (previous_pen == 'down') {
       stroke(0);
       strokeWeight(3.0);
-      line(x, y, x + sketch.dx, y + sketch.dy);
+      line(x, y, x + strokePath.dx, y + strokePath.dy);
     }
-    x += sketch.dx;
-    y += sketch.dy;
-    previous_pen = sketch.pen;
+    // Move the pen
+    x += strokePath.dx;
+    y += strokePath.dy;
+    // The pen state actually refers to the next stroke
+    previous_pen = strokePath.pen;
 
-    if (sketch.pen !== 'end') {
-      sketch = null;
-      model.generate(gotSketch);
+    // If the drawing is complete
+    if (strokePath.pen !== 'end') {
+      strokePath = null;
+      model.generate(gotStroke);
     }
-  } 
+  }
 }
 
-function gotSketch(err, s) {
-  sketch = s;
+// A new stroke path
+function gotStroke(err, s) {
+  strokePath = s;
 }
 
+// The model is ready
 function modelReady() {
-  select('#status').html('Model Loaded');
+  select('#status').html('model ready');
   startDrawing();
 }
-
