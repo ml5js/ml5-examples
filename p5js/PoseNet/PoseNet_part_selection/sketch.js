f82e95b42106cc4e1cacc1ac490beb495d95940c
Daniel Shiffman <daniel.shiffman@gmail.com>
2019-03-15 10:33:36 -0400

    Adjust pose position example #98 (#99)

--- p5js/PoseNet/PoseNet_part_selection/sketch.js
+++ p5js/PoseNet/PoseNet_part_selection/sketch.js
@@ -38,53 +38,25 @@ function mousePressed(){
 
 function draw() {
   image(video, 0, 0, width, height);
-  strokeWeight(2)
+  strokeWeight(2);
 
-  // Create a pink ellipse for the nose
-  fill(213, 0, 143);
-  drawPosition('nose');
+  // For one pose only (use a for loop for multiple poses!)
+  if (poses.length > 0) {
+    let pose = poses[0].pose;
 
-  // Create a yellow ellipse for the right eye
-  fill(255, 215, 0);
-  drawPosition('rightEye');
+    // Create a pink ellipse for the nose
+    fill(213, 0, 143);
+    let nose = pose['nose'];
+    ellipse(nose.x, nose.y, 20, 20);
 
-  // Create a yellow ellipse for the left eye
-  fill(255, 215, 0);
-  drawPosition('leftEye');
-}
+    // Create a yellow ellipse for the right eye
+    fill(255, 215, 0);
+    let rightEye = pose['rightEye'];
+    ellipse(rightEye.x, rightEye.y, 20, 20);
 
-function drawPosition(positionName){
-  
-  // we iterate through all poses in case we have multiple poses
-  // then render an ellipse for the pose by the name
-  /**
-   poses = [{
-      "pose":{
-        "score":0.36784,
-        "keypoints":[ a bunch of k:v, ... ],
-        "skeleton":[ a bunch of k:v, ... ],
-        "nose": {
-          "x": 280.30112659863835,
-          "y": 194.88652961198673,
-          "confidence": 0.9998577833175659
-        },
-        "leftEye": {
-          "x": 327.33367057843395,
-          "y": 147.51913381177326,
-          "confidence": 0.9991821646690369
-        },
-        "rightEye": {
-          "x": 243.02517476054908,
-          "y": 154.62540914846022,
-          "confidence": 0.9997193217277527
-        },
-        ... all of the body parts as properties of the parent pose object
-        }
-      }
-   */
-  poses.forEach(pose => {
-    let currentPose = pose.pose;
-    let selectedPosition = currentPose[positionName];
-    ellipse(selectedPosition.x, selectedPosition.y, 20, 20);
-  })
+    // Create a yellow ellipse for the right eye
+    fill(255, 215, 0);
+    let leftEye = pose['leftEye'];
+    ellipse(leftEye.x, leftEye.y, 20, 20);
+  }
 }
