ea12c30066505df87bcae1f199b5df1cfca1304a
joeyklee <jsphknglee@gmail.com>
2019-03-16 13:23:12 -0400

    updated example with new image and simplified example code

--- p5js/YOLO/YOLO_single_image/sketch.js
+++ p5js/YOLO/YOLO_single_image/sketch.js
@@ -6,48 +6,50 @@ let objects = [];
 let status;
 
 function setup() {
-
-  createCanvas(400, 266);
-  img = createImg('images/turtle.png', imageReady);
+  // createCanvas(400, 266);
+  createCanvas(640, 420);
+  img = createImg('images/cat2.JPG', imageReady);
   img.hide();
-  img.size(400, 266);
+  img.size(640, 420);
 }
 
 // Change the status when the model loads.
 function modelReady() {
   console.log("model Ready!")
   status = true;
-  document.querySelector('#status').innerHTML = 'Model Loaded'
 }
 
 // When the image has been loaded,
 // get a prediction for that image
 function imageReady() {
+  console.log('Detecting') 
   yolo.detect(img, gotResult);
 }
 
 // A function to run when we get any errors and the results
 function gotResult(err, results) {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
-  objects = results.slice(0);
+  console.log(results)
+  objects = results;
 }
 
 
 function draw() {
-  // image(video, 0, 0, width, height);
+  // unless the model is loaded, do not draw anything to canvas
   if (status != undefined) {
     image(img, 0, 0)
 
     for (let i = 0; i < objects.length; i++) {
       noStroke();
       fill(0, 255, 0);
-      text(objects[i].className, objects[i].x * width, objects[i].y * height - 5);
+      text(objects[i].label + " " + nfc(objects[i].confidence * 100.0, 2) + "%", objects[i].x * width + 5, objects[i].y * height + 15);
       noFill();
       strokeWeight(4);
       stroke(0, 255, 0);
       rect(objects[i].x * width, objects[i].y * height, objects[i].w * width, objects[i].h * height);
     }
+
   }
 }
