96ba787073dfa2a3ba1c558f842d651abf1c963a
joeyklee <jsphknglee@gmail.com>
2019-04-05 11:36:35 -0400

    added dev and production modes for development and release

--- scripts/update-ml5-reference.js
+++ scripts/update-ml5-reference.js
@@ -6,23 +6,32 @@ const { parse } = require('node-html-parser');
 const baseurl = path.resolve(__dirname, "..");
 const ml5version = process.argv[2] || '0.2.3'
 
+let ml5src;
+if(process.env.NODE_ENV && ['development', 'dev', 'DEVELOPMENT'].includes(process.env.NODE_ENV) === true ){
+    console.log(`setting src for ${process.env.NODE_ENV}`)
+    ml5src = `src="localhost:8080/ml5.js" type="text/javascript"`
+} else {
+    console.log(`setting src for production`)
+    ml5src = `src="https://unpkg.com/ml5@${ml5version}/dist/ml5.min.js" type="text/javascript"`
+}
+
 // run the functions
-make("/javascript")
-make("/p5js")
+make("/javascript", ml5src);
+make("/p5js", ml5src);
 
 /**
  * Take the relative path to the examples directory and 
  * runs the process
  * @param {*} examplesDir 
  */
-function make(examplesDir){
+function make(examplesDir, ml5src){
     recursive(baseurl + examplesDir, (err, files) => {
         // let indexFiles = []; 
         files.forEach(file => {
             const fileName = file.split('/').pop()
             if(fileName.endsWith('.html')){
                 // indexFiles.push(file)
-                updateMl5Reference(file)
+                updateMl5Reference(file, ml5src)
             }
         })
         console.log('🌈 done!')
@@ -35,7 +44,7 @@ function make(examplesDir){
  * works the DOM to put in our script
  * @param {*} filePath 
  */
-function updateMl5Reference(filePath){
+function updateMl5Reference(filePath, ml5src){
     let pos;
     let el = parse(fs.readFileSync(filePath, 'utf8'));
     let scripts = el.querySelectorAll('script')
@@ -45,7 +54,7 @@ function updateMl5Reference(filePath){
             return item
         }
     })
-    selectedRef.rawAttrs = `src="https://unpkg.com/ml5@${ml5version}/dist/ml5.min.js" type="text/javascript"`
+    selectedRef.rawAttrs = ml5src
     scripts[pos] = selectedRef;
 
     fs.writeFileSync(filePath, el, 'utf8');
