b830161d531856889288af338bbae0baa8fa4c56
joeyklee <jsphknglee@gmail.com>
2019-04-05 10:48:52 -0400

    added javascript dir to script and added comments

--- scripts/update-ml5-reference.js
+++ scripts/update-ml5-reference.js
@@ -6,18 +6,35 @@ const { parse } = require('node-html-parser');
 const baseurl = path.resolve(__dirname, "..");
 const ml5version = process.argv[2] || '0.2.3'
 
-recursive(baseurl + "/p5js", (err, files) => {
-    // let indexFiles = []; 
-    files.forEach(file => {
-        const fileName = file.split('/').pop()
-        if(fileName.endsWith('.html')){
-            // indexFiles.push(file)
-            updateMl5Reference(file)
-        }
-    })
-    console.log('🌈 done!')
-})
+// run the functions
+make("/javascript")
+make("/p5js")
 
+/**
+ * Take the relative path to the examples directory and 
+ * runs the process
+ * @param {*} examplesDir 
+ */
+function make(examplesDir){
+    recursive(baseurl + examplesDir, (err, files) => {
+        // let indexFiles = []; 
+        files.forEach(file => {
+            const fileName = file.split('/').pop()
+            if(fileName.endsWith('.html')){
+                // indexFiles.push(file)
+                updateMl5Reference(file)
+            }
+        })
+        console.log('🌈 done!')
+    })    
+}
+
+
+/**
+ * takes the filepath to the index.hml file
+ * works the DOM to put in our script
+ * @param {*} filePath 
+ */
 function updateMl5Reference(filePath){
     let pos;
     let el = parse(fs.readFileSync(filePath, 'utf8'));
