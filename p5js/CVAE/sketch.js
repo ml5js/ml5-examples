5c13071571b6c35e2d8a93cfcf7bafd1e041a8ca
Joey Lee <joeyklee@users.noreply.github.com>
2019-04-19 16:25:53 -0400

    added new cvae example (#123)

--- p5js/CVAE/sketch.js
+++ p5js/CVAE/sketch.js
@@ -8,27 +8,47 @@ ml5 Example
 CVAE example using p5.js
 === */
 let cvae;
+let labelElement;
+let generatedImage;
+let labelToDraw = 0; // apple
 
 function setup() {
-    createCanvas(640, 480);
-    
+    createCanvas(400, 400);
+
+    labelName = createP('')
     //Create a new instance with pretrained model
-    cvae = ml5.CVAE('./model/quick_draw/manifest.json', modelLoaded)
+    cvae = ml5.CVAE('./model/quick_draw/manifest.json', generateImage)
 }
 
-// when mouse moved and the offset is greater than 20 we re-generate a new image
+// Generate a new image on mouseMoved()
 function mouseMoved() {
-    if (Math.abs(mouseX - pmouseX) >= 20 && cvae.ready) {
-        cvae.generate(cvae.labels[0], (err, res) => {
-            image(res.image, 0, 0)
-        });
+    generateImage()
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && labelToDraw < cvae.labels.length - 1){
+        labelToDraw++;
+        generateImage()
+    }
+    if(keyCode === DOWN_ARROW && labelToDraw > 0){
+        labelToDraw--;
+        generateImage()
     }
 }
 
-
-function modelLoaded() {
-    cvae.generate(cvae.labels[0], (err, res) => {
-        image(res.image, 0, 0)
+function generateImage() {
+    // label 6: apple
+    cvae.generate(cvae.labels[labelToDraw], (err, res) => {
+        generatedImage = res.image;
     });
-    
+}
+
+function draw() {
+    background(200)
+    if (generatedImage) {
+        labelName.html(cvae.labels[labelToDraw])
+        image(generatedImage, 0, 0, height, height)
+
+    }
+
 }
