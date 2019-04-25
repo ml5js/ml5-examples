763a50570b423c4a563f565b95b241b6f36f6d64
shiffman <daniel@shiffman.net>
2019-04-25 11:26:06 -0400

    simplfiying the sentiment example

--- p5js/Sentiment/sketch.js
+++ p5js/Sentiment/sketch.js
@@ -5,36 +5,33 @@ let inputBox;
 let sentimentResult;
 
 function setup() {
-    // update all html elements
-    statusEl = createP('Loading Model...');
-    // initialize sentiment
-    sentiment = ml5.sentiment('movieReviews', modelReady);
+  noCanvas();
+  // initialize sentiment
+  sentiment = ml5.sentiment('movieReviews', modelReady);
 
-    // setup the html environment
-    submitBtn = createButton('submit').parent('myForm');
-    inputBox = select('#inputBox');
-    sentimentResult = createP('sentiment score:').parent('myForm');
+  // setup the html environment
+  statusEl = createP('Loading Model...');
+  inputBox = createInput('Today is the happiest day and is full of rainbows!');
+  inputBox.attribute('size', '75');
+  submitBtn = createButton('submit');
+  sentimentResult = createP('sentiment score:');
 
-    // predicting the sentiment on mousePressed()
-    submitBtn.mousePressed(getSentiment);
-
+  // predicting the sentiment on mousePressed()
+  submitBtn.mousePressed(getSentiment);
 }
 
-function getSentiment(){
-    // get the values from the input
-    const text = inputBox.value();
-    
-    // make the prediction
-    const prediction = sentiment.predict(text);
+function getSentiment() {
+  // get the values from the input
+  const text = inputBox.value();
 
-    // display sentiment result on html page
-    sentimentResult.html('Sentiment score: ' + prediction.score);
+  // make the prediction
+  const prediction = sentiment.predict(text);
+
+  // display sentiment result on html page
+  sentimentResult.html('Sentiment score: ' + prediction.score);
 }
 
 function modelReady() {
-    // model is ready
-    statusEl.html('model loaded');
+  // model is ready
+  statusEl.html('model loaded');
 }
-
-
-
