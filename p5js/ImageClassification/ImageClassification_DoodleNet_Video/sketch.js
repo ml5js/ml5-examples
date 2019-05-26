fc1078494caa0554859d3f2eaafdc0f1d38c2b4a
Yining Shi <ys2189@nyu.edu>
2019-05-25 21:53:41 -0400

    added doodlenet video example

--- p5js/ImageClassification/ImageClassification_DoodleNet_Video/sketch.js
+++ p5js/ImageClassification/ImageClassification_DoodleNet_Video/sketch.js
@@ -5,51 +5,42 @@
 
 /* ===
 ml5 Example
-Canvas Image Classification using DoodleNet and p5.js
+Webcam Image Classification using DoodleNet and p5.js
 This example uses a callback pattern to create the classifier
 === */
 
 // Initialize the Image Classifier method with DoodleNet.
 let classifier;
 
-// A variable to hold the canvas image we want to classify
-let canvas;
+// A variable to hold the Webcam video we want to classify
+let video;
 
 // Two variable to hold the label and confidence of the result
 let label;
 let confidence;
 
 function preload() {
+  // Create a camera input
+  video = createCapture(VIDEO, {
+    video: {
+      width: 280,
+      height: 280,
+      aspectRatio: 1
+    } 
+  });
   // Load the DoodleNet Image Classification model
-  classifier = ml5.imageClassifier('DoodleNet');
+  classifier = ml5.imageClassifier('DoodleNet', video);
 }
 
 function setup() {
-  // Create a canvas with 280 x 280 px
-  canvas = createCanvas(280, 280);
-  // Set canvas background to white
-  background(255);
-  // Whenever mouseReleased event happens on canvas, call "classifyCanvas" function
-  canvas.mouseReleased(classifyCanvas);
-  // Create a clear canvas button
-  createClearBtn();
   // Create a div to hold results
   createResDiv();
+  classifyVideo();
 }
 
-function draw() {
-  // Set stroke weight to 10
-  strokeWeight(15);
-  // Set stroke color to black
-  stroke(0);
-  // If mouse is pressed, draw line between previous and current mouse positions
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
-}
-
-function classifyCanvas() {
-  classifier.classify(canvas, gotResult);
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.classify(video, gotResult);
 }
 
 // A function to run when we get any errors and the results
@@ -66,16 +57,8 @@ function gotResult(error, results) {
   const confidences = results.map(r => nf(r.confidence, 0, 2));
   label.html('Label: ' + labels);
   confidence.html('Confidence: ' + confidences);
-}
-
-function createClearBtn() {
-  let button = createButton('Clear Canvas');
-  button.position(7, 44);
-  button.mousePressed(clearCanvas);
-}
-
-function clearCanvas() {
-  background(255);
+  // Call classifyVideo again
+  classifyVideo();
 }
 
 function createResDiv() {
