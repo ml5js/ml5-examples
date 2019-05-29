840994bb96e20e01af393f1850496beb6552c663
joeyklee <jsphknglee@gmail.com>
2019-05-29 12:59:12 -0400

    add segmentation.image - TODO: fix repeating image issue

--- p5js/BodyPix/BodyPix_Webcam/sketch.js
+++ p5js/BodyPix/BodyPix_Webcam/sketch.js
@@ -3,22 +3,38 @@ console.log('bodyPix', ml5.bodyPix);
 
 let bodypix;
 let video;
+let segmentation;
+let img;
 function setup(){
-    createCanvas(640, 480);
+    createCanvas(320, 240);
 
     // load up your video
     video = createCapture(VIDEO);
     video.size(width, height);
-    video.hide(); // Hide the video element, and just show the canvas
+    // video.hide(); // Hide the video element, and just show the canvas
     bodypix = ml5.bodyPix(video, modelReady)
-    
+    // background(0);
 }
 
 function modelReady(){
     console.log('ready!')
+    console.log(bodypix)
+    bodypix.segment(video, gotResults)
 }
 
-function draw(){
-    background(200);
+function gotResults(err, result){
+    if(err){
+        console.log(err)
+        return
+    }
+    // console.log(result);
+    segmentation = result;
+    
+    background(255);
 
+    // TODO: image seems to be repeating 4x
+    image(segmentation.image, 0, 0, width, height)
+
+    bodypix.segment(video, gotResults)
+    
 }
