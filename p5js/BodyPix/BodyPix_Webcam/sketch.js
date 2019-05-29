a62c3477a2e1a59f20e68af666c615ccbb37f71b
joeyklee <jsphknglee@gmail.com>
2019-05-29 10:35:46 -0400

    added video and load model

--- p5js/BodyPix/BodyPix_Webcam/sketch.js
+++ p5js/BodyPix/BodyPix_Webcam/sketch.js
@@ -1,8 +1,21 @@
 
 console.log('bodyPix', ml5.bodyPix);
 
+let bodypix;
+let video;
 function setup(){
     createCanvas(640, 480);
+
+    // load up your video
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide(); // Hide the video element, and just show the canvas
+    bodypix = ml5.bodyPix(video, modelReady)
+    
+}
+
+function modelReady(){
+    console.log('ready!')
 }
 
 function draw(){
