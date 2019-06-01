7a9714dc4114acc521add66828492eb2c5dab822
Yining Shi <ys2189@nyu.edu>
2019-06-01 19:01:01 -0400

    imageClassifier load a model from url

---
+++ p5js/ImageClassification/ImageClassification_Video_Load/sketch.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trianed customized model and p5.js
+This example uses a callback pattern to create the classifier
+=== */
+
+const checkpoint = 'https://storage.googleapis.com/tm-pro-a6966.appspot.com/eyeo-test-yining/model.json';
+let classifier;
+let video;
+let resultsP;
+
+function setup() {
+  noCanvas();
+  // Create a camera input
+  video = createCapture(VIDEO);
+  // Initialize the Image Classifier method with a pre-trained customized model and the video as the second argument
+  classifier = ml5.imageClassifier(checkpoint, video, modelReady);
+  resultsP = createP('Loading model and video...');
+}
+
+function modelReady() {
+  console.log('Model Ready');
+  classifyVideo();
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.classify(gotResult);
+}
+
+// When we get a result
+function gotResult(err, results) {
+  // The results are in an array ordered by confidence.
+  resultsP.html('Label: ' + results[0].label + ' ' + nf(results[0].confidence, 0, 2));
+  classifyVideo();
+}
