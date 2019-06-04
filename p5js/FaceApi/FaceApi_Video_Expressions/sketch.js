28d472a3ace878b2228f7c6974b33813803868db
joeyklee <jsphknglee@gmail.com>
2019-06-04 16:12:02 -0400

    added face-expressions demo

---
+++ p5js/FaceApi/FaceApi_Video_Expressions/sketch.js
@@ -0,0 +1,54 @@
+let faceapi;
+let video;
+let detections;
+let img;
+
+const options = {
+    outputStride: 8, // 8, 16, or 32, default is 16
+    segmentationThreshold: 0.3 // 0 - 1, defaults to 0.5 
+}
+
+function setup() {
+    createCanvas(400, 240);
+
+    // load up your video
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    // video.hide(); // Hide the video element, and just show the canvas
+    faceapi = ml5.faceApi('/models', video, modelReady)
+    textAlign(RIGHT);
+}
+
+function modelReady() {
+    console.log('ready!')
+    console.log(faceapi)
+    faceapi.classifyExpressions(gotResults)
+
+}
+
+function gotResults(err, result) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    // console.log(result)
+    detections = result;
+
+    background(255);
+    if (detections) {
+        if (detections.length > 0) {
+            const {expressions} = detections[0]
+            let keys = Object.keys(expressions);
+            keys.forEach( (item, idx) => {
+                fill(0)
+                // text(`${expression}: ${probability}`, 20, idx*20 )
+                textSize(12)
+                text(`${item}:`, 70, idx * 20 + 20)
+                const val = map(expressions[item], 0, 1, 0, width / 2)
+                rect(80, idx * 20 + 10, val, 15)
+            })
+        }
+
+    }
+    faceapi.classifyExpressions(gotResults)
+}
