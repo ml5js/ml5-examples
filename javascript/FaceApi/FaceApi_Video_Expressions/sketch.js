491f8caad1a201b74f537f9cb6027e88999a341d
joeyklee <jsphknglee@gmail.com>
2019-08-09 14:29:01 -0400

    wip expressions demo

--- javascript/FaceApi/FaceApi_Video_Expressions/sketch.js
+++ javascript/FaceApi/FaceApi_Video_Expressions/sketch.js
@@ -1,6 +1,9 @@
 let faceapi;
 let video;
 let detections;
+let width = 200;
+let height = 200;
+let canvas;
 
 // by default all options are set to true
 const detection_options = {
@@ -9,16 +12,21 @@ const detection_options = {
     withDescriptors: true,
 }
 
-function setup() {
-    createCanvas(400, 240);
+async function make() {
+    canvas = createCanvas(width, height);
 
     // load up your video
-    video = createCapture(VIDEO);
-    video.size(width, height);
+    video = await getVideo();
+    
     // video.hide(); // Hide the video element, and just show the canvas
-    faceapi = ml5.faceApi( video, detection_options, modelReady)
-    textAlign(RIGHT);
+    faceapi = await ml5.faceApi( video, detection_options, modelReady)
+    // textAlign(RIGHT);
 }
+
+// call app.map.init() once the DOM is loaded
+window.addEventListener('DOMContentLoaded', function() {
+    make();
+  });
 
 function modelReady() {
     console.log('ready!')
@@ -28,6 +36,25 @@ function modelReady() {
 
 }
 
+
+// Helper Functions
+async function getVideo(){
+    // Grab elements, create settings, etc.
+    const videoElement = document.createElement('video');
+    videoElement.setAttribute("style", "display: none;"); 
+    videoElement.width = width;
+    videoElement.height = height;
+    document.body.appendChild(videoElement);
+
+    // Create a webcam capture
+    const capture = await navigator.mediaDevices.getUserMedia({ video: true })
+    videoElement.srcObject = capture;
+    videoElement.play();
+
+    return videoElement
+}
+
+
 function gotResults(err, result) {
     if (err) {
         console.log(err)
@@ -36,21 +63,39 @@ function gotResults(err, result) {
     // console.log(result)
     detections = result;
 
-    background(255);
+    // Clear part of the canvas
+    canvas.fillStyle = "#FFFFFF"
+    canvas.fillRect(0,0, width, height);
+
     if (detections) {
         if (detections.length > 0) {
             const expressions = detections[0].expressions;
 
             let keys = Object.keys(expressions);      
             for(let i = 0; i < keys.length; i++){
-                fill(0)
-                textSize(12)
-                text(`${keys[i]}:`, 70, i * 20 + 20)
-                const val = map(expressions[keys[i]], 0, 1, 0, width / 2)
-                rect(80, i * 20 + 10, val, 15)
+                canvas.font = "10px Arial";
+                canvas.fillText(`${keys[i]}:`, 70, i * 20 + 20)
+                const val = scale(expressions[keys[i]], 0, 1, 0, width / 2)
+                canvas.fillStyle = "#000000"
+                canvas.fillRect(80, i*20 + 10, val, 15);
             }
         }
 
     }
     faceapi.detect( gotResults)
 }
+
+function scale (num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
+
+
+// Helper Functions
+function createCanvas(w, h){
+    const canvasElement = document.createElement("canvas"); 
+    canvasElement.width  = w;
+    canvasElement.height = h;
+    document.body.appendChild(canvasElement);
+    const canvas = canvasElement.getContext("2d");
+    return canvas;
+  }
