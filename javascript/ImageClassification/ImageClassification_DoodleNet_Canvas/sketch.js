857bb60c5c36aca828279dc114ef7bd118a692c7
joeyklee <jsphknglee@gmail.com>
2019-08-14 18:04:05 -0400

    added ImageClassification_DoodleNet_Canvas

--- javascript/ImageClassification/ImageClassification_DoodleNet_Canvas/sketch.js
+++ javascript/ImageClassification/ImageClassification_DoodleNet_Canvas/sketch.js
@@ -5,7 +5,7 @@
 
 /* ===
 ml5 Example
-Canvas Image Classification using DoodleNet and p5.js
+Canvas Image Classification using DoodleNet
 This example uses a callback pattern to create the classifier
 === */
 
@@ -18,42 +18,106 @@ let canvas;
 // Two variable to hold the label and confidence of the result
 let label;
 let confidence;
+let button
+let width = 280;
+let height = 280;
 
-function preload() {
-  // Load the DoodleNet Image Classification model
-  classifier = ml5.imageClassifier('DoodleNet');
-}
+let pX = null;
+let pY = null;
+let x = null;
+let y = null;
 
-function setup() {
+let mouseDown = false;
+
+setup();
+async function setup() {
+  canvas = await createCanvas(width, height);
+  classifier = await ml5.imageClassifier('DoodleNet', onModelReady);
   // Create a canvas with 280 x 280 px
-  canvas = createCanvas(280, 280);
-  // Set canvas background to white
-  background(255);
-  // Whenever mouseReleased event happens on canvas, call "classifyCanvas" function
-  canvas.mouseReleased(classifyCanvas);
+
+  document.querySelector('canvas').addEventListener('mousemove', onMouseUpdate);
+  document.querySelector('canvas').addEventListener('mousedown', onMouseDown);
+  document.querySelector('canvas').addEventListener('mouseup', onMouseUp);
+
   // Create a clear canvas button
-  let button = createButton('Clear Canvas');
-  button.position(7, 44);
-  button.mousePressed(clearCanvas);
+  button = document.querySelector("#clearBtn");
+
+  button.addEventListener('click', clearCanvas);
   // Create 'label' and 'confidence' div to hold results
-  label = createDiv('Label: ...');
-  confidence = createDiv('Confidence: ...');
+  label = document.querySelector("#label");
+  confidence = document.querySelector("#confidence");
+
+  requestAnimationFrame(draw)
 }
+
+function onModelReady() {
+  console.log('ready!')
+}
+
 
 function clearCanvas() {
-  background(255);
+  canvas.fillStyle = '#ebedef'
+  canvas.fillRect(0, 0, width, height);
 }
 
+
+let request;
+
 function draw() {
-  // Set stroke weight to 10
-  strokeWeight(15);
-  // Set stroke color to black
-  stroke(0);
-  // If mouse is pressed, draw line between previous and current mouse positions
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
+  request = requestAnimationFrame(draw)
+
+  if (pX == null || pY == null) {
+    pX = x
+    pY = y
   }
+  x = x
+  y = y
+
+
+  // Set stroke weight to 10
+  canvas.lineWidth = 10;
+  // Set stroke color to black
+  canvas.strokeStyle = "#000000";
+  // If mouse is pressed, draw line between previous and current mouse positions
+  if (mouseDown === true) {
+    canvas.beginPath();
+    canvas.lineCap = "round";
+    canvas.moveTo(x, y);
+    canvas.lineTo(pX, pY);
+    canvas.stroke();
+  }
+
+  pX = x;
+  pY = y;
 }
+
+
+
+function onMouseDown(e) {
+  mouseDown = true;
+}
+
+function onMouseUp(e) {
+  mouseDown = false;
+  classifyCanvas();
+}
+
+function onMouseUpdate(e) {
+  var pos = getMousePos(document.querySelector('canvas'), e);
+  x = pos.x;
+  y = pos.y;
+
+}
+
+function getMousePos(canvas, e) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  };
+}
+
+
 
 function classifyCanvas() {
   classifier.classify(canvas, gotResult);
@@ -68,6 +132,18 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   console.log(results);
   // Show the first label and confidence
-  label.html('Label: ' + results[0].label);
-  confidence.html('Confidence: ' + nf(results[0].confidence, 0, 2)); // Round the confidence to 0.01
+  label.innerHTML = 'Label: ' + results[0].label;
+  confidence.innerHTML = 'Confidence: ' + results[0].confidence.toFixed(4)
+}
+
+
+function createCanvas(w, h) {
+  const canvasElement = document.createElement("canvas");
+  canvasElement.width = w;
+  canvasElement.height = h;
+  document.body.appendChild(canvasElement);
+  const canvas = canvasElement.getContext("2d");
+  canvas.fillStyle = '#ebedef'
+  canvas.fillRect(0, 0, width, height);
+  return canvas;
 }
