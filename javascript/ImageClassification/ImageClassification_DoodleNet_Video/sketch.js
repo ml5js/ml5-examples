d4bfa26319aa5056849f35d285b56cc1e701dcf8
joeyklee <jsphknglee@gmail.com>
2019-08-15 13:49:39 -0400

    add doodlenet_video - NOT WORKING

--- javascript/ImageClassification/ImageClassification_DoodleNet_Video/sketch.js
+++ javascript/ImageClassification/ImageClassification_DoodleNet_Video/sketch.js
@@ -5,7 +5,7 @@
 
 /* ===
 ml5 Example
-Webcam Image Classification using DoodleNet and p5.js
+Webcam Image Classification using DoodleNet
 This example uses a callback pattern to create the classifier
 === */
 
@@ -18,27 +18,28 @@ let video;
 // Two variable to hold the label and confidence of the result
 let label;
 let confidence;
+let canvas;
+let ctx;
 
-function preload() {
-  // Create a camera input
-  video = createCapture(VIDEO, {
-    video: {
-      width: 280,
-      height: 280,
-      aspectRatio: 1
-    } 
-  });
-  // Load the DoodleNet Image Classification model
-  classifier = ml5.imageClassifier('DoodleNet', video);
-}
-
-function setup() {
+async function setup() {
   // Create a 'label' and 'confidence' div to hold results
-  label = createDiv('Label: ...');
-  confidence = createDiv('Confidence: ...');
+  label = document.querySelector('#label');
+  confidence = document.querySelector('#confidence');
+  // Create a camera input
+  video = document.querySelector('#myvideo');
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true  })
+  video.height = 280;
+  video.width = 280;
+  video.srcObject = stream;
+  video.play();
+
+  classifier = await ml5.imageClassifier('DoodleNet', video);
 
   classifyVideo();
+  
 }
+setup();
+
 
 // Get a prediction for the current video frame
 function classifyVideo() {
@@ -54,8 +55,8 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   console.log(results);
   // Show the first label and confidence
-  label.html('Label: ' + results[0].label);
-  confidence.html('Confidence: ' + nf(results[0].confidence, 0, 2)); // Round the confidence to 0.01
+  label.innerHTML =  results[0].label;
+  confidence.innerHTML =  results[0].confidence.toFixed(4); // Round the confidence to 0.01
   // Call classifyVideo again
   classifyVideo();
 }
