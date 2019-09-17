7bdfa758dbd57e279f8f7eebd1fd34f034b874a5
joeyklee <jsphknglee@gmail.com>
2019-09-17 15:30:35 -0400

    updates example

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -14,16 +14,31 @@ let nn_options = {
 function setup(){
     nn = ml5.neuralNetwork(nn_options);
     
-    nn.loadData(TRAIN_DATA_PATH, async function(err, results){
-        data = results;
-        normalizedData = nn.normalize(data);
-
-        await nn.train(normalizedData.inputs, normalizedData.labels)
-
-        console.log(normalizedData);
-    });
+    nn.loadData(TRAIN_DATA_PATH, dataLoaded);
 
     
     noLoop();
 }
 
+function dataLoaded(err, results){
+    data = results
+    normalizedData = nn.normalize(data);
+
+    nn.train(normalizedData.inputs, normalizedData.labels, finishedTraining)
+}
+
+
+function finishedTraining(err, results){
+    predict();
+}
+
+function predict(){
+    const age = floor( random(1, 80) ) 
+    const fare = floor( random(0, 500) )
+    const is_female = floor( random(0, 2) )
+    // console.log([age, fare, is_female])
+
+    nn.predict([age, fare, is_female], function(err, prediction){
+        console.log([age, fare, is_female],prediction.output)
+    })
+}
