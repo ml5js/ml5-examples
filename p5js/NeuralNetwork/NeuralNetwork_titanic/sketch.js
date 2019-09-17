016d449a404b5c6e12c85d2437d9e7940a2adfa9
joeyklee <jsphknglee@gmail.com>
2019-09-17 14:53:37 -0400

    adds normalize data function

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -3,7 +3,7 @@ let nn;
 const TRAIN_DATA_PATH = 'data/titanic_train.csv';
 const TEST_DATA_PATH = 'data/titanic_test.csv';
 let data;
-
+let normalizedData;
 let nn_options = {
     inputs:3,
     outputs:2,
@@ -14,10 +14,11 @@ let nn_options = {
 function setup(){
     nn = ml5.neuralNetwork(nn_options);
     
-    nn.loadData(TRAIN_DATA_PATH, function(err, results){
+    nn.loadData(TRAIN_DATA_PATH, async function(err, results){
         data = results;
-        
-        console.log(data);
+        normalizedData = nn.normalize(data);
+
+        console.log(normalizedData);
     });
 
     
