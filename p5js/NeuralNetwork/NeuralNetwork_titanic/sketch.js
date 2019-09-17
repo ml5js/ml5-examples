408243ea0fd043eb4837632e885f4f4dcc7fcdea
shiffman <daniel@shiffman.net>
2019-09-17 16:41:48 -0400

    speculative ideas with example

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -2,43 +2,46 @@ let nn;
 
 const TRAIN_DATA_PATH = 'data/titanic_train.csv';
 const TEST_DATA_PATH = 'data/titanic_test.csv';
+
 let data;
 let normalizedData;
-let nn_options = {
-    inputs:3,
-    outputs:2,
-    outputKeys: ['survived'],
-    inputKeys: ['age', 'fare', 'is_female'],
+
+function setup() {
+  let nn_options = {
+    inputs: 3,
+    outputs: 2,
     task: 'classification'
-}
-function setup(){
-    nn = ml5.neuralNetwork(nn_options);
-    
-    nn.loadData(TRAIN_DATA_PATH, dataLoaded);
+  }
+  nn = ml5.neuralNetwork(nn_options);
 
-    
-    noLoop();
-}
-
-function dataLoaded(err, results){
-    data = results
-    normalizedData = nn.normalize(data);
-
-    nn.train(normalizedData.inputs, normalizedData.labels, finishedTraining)
+  let data_options = {
+    path: TRAIN_DATA_PATH,
+    inputKeys: ['age', 'fare', 'is_female'],
+    outputKeys: ['survived']
+  };
+  nn.loadData(data_options, dataLoaded);
 }
 
-
-function finishedTraining(err, results){
-    predict();
+function dataLoaded(err, results) {
+  data = results;
+  normalizedData = nn.normalize(data);
+  console.log(normalizedData);
+  nn.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
 }
 
-function predict(){
-    const age = floor( random(1, 80) ) 
-    const fare = floor( random(0, 500) )
-    const is_female = floor( random(0, 2) )
-    // console.log([age, fare, is_female])
 
-    nn.predict([age, fare, is_female], function(err, prediction){
-        console.log([age, fare, is_female],prediction.output)
-    })
+function finishedTraining(err, results) {
+  predict();
+}
+
+function predict() {
+  const age = floor(random(1, 80))
+  const fare = floor(random(0, 500))
+  const is_female = floor(random(0, 2))
+  // console.log([age, fare, is_female])
+
+  nn.predict([age, fare, is_female], function (err, prediction) {
+    console.log(prediction);
+    console.log([age, fare, is_female], prediction.output);
+  })
 }
