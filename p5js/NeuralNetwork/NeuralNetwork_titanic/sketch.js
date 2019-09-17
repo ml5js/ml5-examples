cbcc27f0100fb4787e955af7f858bff1e01456e3
joeyklee <jsphknglee@gmail.com>
2019-09-17 14:08:59 -0400

    adds data, constructor, and laodData

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -1,13 +1,26 @@
 let nn;
 
+const TRAIN_DATA_PATH = 'data/titanic_train.csv';
+const TEST_DATA_PATH = 'data/titanic_test.csv';
+let data;
+
 let nn_options = {
     inputs:3,
     outputs:2,
+    outputKeys: ['survived'],
+    inputKeys: ['age', 'fare', 'is_female'],
     task: 'classification'
 }
 function setup(){
-    nn = ml5.neuralNetwork(nn_options)
-    console.log(nn);
+    nn = ml5.neuralNetwork(nn_options);
+    
+    nn.loadData(TRAIN_DATA_PATH, function(err, results){
+        data = results;
+        
+        console.log(data);
+    });
+
+    
     noLoop();
 }
 
