16274e218db05af74bd05bc3a5d4f9e637b75fdd
shiffman <daniel@shiffman.net>
2019-09-17 21:10:34 -0400

    refactor of titanic example

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -1,49 +1,69 @@
-let nn;
-
-const TRAIN_DATA_PATH = 'data/titanic_train.csv';
-const TEST_DATA_PATH = 'data/titanic_test.csv';
-
+let classifier;
 let data;
 let normalizedData;
+let submitButton;
 
 function setup() {
-  let nn_options = {
+  // Create a classification neural network
+  let nnOptions = {
     inputs: 3,
     outputs: 2,
-    task: 'classification'
-  }
-  nn = ml5.neuralNetwork(nn_options);
+    task: 'classification',
+  };
+  // TODO: debug tf.vis off by default?
+  classifier = ml5.neuralNetwork(nnOptions);
 
-  let data_options = {
-    path: TRAIN_DATA_PATH,
+  // Load data from a CSV into it
+  let dataOptions = {
+    dataUrl: 'data/titanic_train.csv',
     inputKeys: ['age', 'fare', 'is_female'],
     outputKeys: ['survived']
   };
-  nn.loadData(data_options, dataLoaded);
+  classifier.loadData(dataOptions, dataLoaded);
+
+  submitButton = select('#submit');
+  submitButton.mousePressed(predict);
+  submitButton.hide();
+
 }
 
 function dataLoaded(err, results) {
   data = results;
-  normalizedData = nn.normalize(data);
+  normalizedData = classifier.normalize(data);
   console.log(normalizedData);
-  nn.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
+  // TODO: epochs could probably be specified here?
+  // TODO: ml5 is auto printing loss to console, should have an option epoch event here?
+  classifier.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
 }
 
-
 function finishedTraining(err, results) {
-  predict();
+  console.log('training complete');
+  submitButton.show();
+  select('#result').html('training complete.');
 }
 
 function predict() {
-  const age = floor(random(1, 80))
-  const fare = floor(random(0, 500))
-  const is_female = floor(random(0, 2))
-  console.log([age, fare, is_female])
+  let age = parseInt(select('#age').value());
+  let fare = parseInt(select('#fare').value());
+  // TODO: allow for string labels
+  let is_female = parseInt(select('#is_female').value());
 
   let inputs = [age, fare, is_female];
-  nn.predict(inputs, function (err, results) {
-    console.log(results);
-    console.log(results[0].label);
-    console.log(results[0].confidence);
-  })
+  classifier.predict(inputs, function (err, results) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(results);
+      if (results.output[0] > 0.5) {
+        select('#result').html('prediction: they died');
+      } else {
+        select('#result').html('prediction: they lived');
+      }
+      // TODO:
+      // This would only work for classification but maybe we should reformat the output into
+      // sorted labels and confidence scores?
+      // console.log(results[0].label);
+      // console.log(results[0].confidence);
+    }
+  });
 }
