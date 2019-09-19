bd12e04efbf9150028af0cacdf5335758fd333c1
Joey Lee <joeyklee@users.noreply.github.com>
2019-09-19 17:36:16 -0400

    Adds titanic example to match diy-nn-with-data (#188)
    * adds experimental csv and specified string as output
    
    * working with titanic
    
    * update examples - TODO: feed in normalized vals for titanic inputs
    
    * adds simple regression example placeholder
    
    * adds simple regression example
    
    * comments train with options
    
    * moves batchSize and epoch options
    
    * change to .classify()
    
    * updates co2 training sketch with predict vals from 0 - 1

---
+++ p5js/NeuralNetwork/NeuralNetwork_Simple-Regression/sketch.js
@@ -0,0 +1,67 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Image classification using MobileNet and p5.js
+This example uses a callback pattern to create the classifier
+=== */
+let nn;
+
+
+
+function setup(){
+  createCanvas(400, 400);
+  nn = ml5.neuralNetwork(1, 1);
+
+
+  console.log(nn)
+  createTrainingData();
+
+  nn.data.shuffle();
+  nn.data.normalize();
+
+  const trainingOptions={
+    batchSize: 24,
+    epochs: 200
+  }
+  
+  // nn.train(trainingOptions,finishedTraining); // if you want to change the training options
+  nn.train(finishedTraining); // use the default training options
+}
+
+function finishedTraining(){
+  for(let i = 0; i < 1; i+= 0.1){
+    nn.predict([i], (err, results) => {
+      let prediction = results.output[0]
+      let x = map(i, 0, 1, 0, width)
+      let y = map(prediction, 0, 1, 0, width)
+      fill(255, 0, 0);
+      rectMode(CENTER);
+      rect(x, y, 10, 10);
+    })
+  }
+  
+}
+
+function createTrainingData(){
+  for(let i = 0; i < width; i+=10){
+    const iters = floor(random(5, 20))
+    const spread = 50;
+    for(let j = 0; j < iters; j++){
+      
+      let data = [{input0: i}, {output0: height - i + floor(random(-spread, spread)) }]
+      fill(0, 0, 255);
+      ellipse(data[0].input0, data[1].output0, 10, 10)
+      nn.data.addData(data[0], data[1])
+    }
+    
+  }
+}
+
+// function draw(){
+//   background(200)
+  
+// }
--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -1,55 +1,55 @@
-let classifier;
-let data;
-let normalizedData;
+let nn;
 let submitButton;
+let nnOptions = {
+  dataUrl: 'data/titanic_train-experimental.csv',
+  inputs: ['age', 'fare', 'is_female'],
+  outputs: ['survived_string'],
+  task: 'classification'
+};
 
-function setup() {
-  // Create a classification neural network
-  let nnOptions = {
-    inputs: 3,
-    outputs: 2,
-    task: 'classification',
-  };
-  // TODO: debug tf.vis off by default?
-  classifier = ml5.neuralNetwork(nnOptions);
-
-  // Load data from a CSV into it
-  let dataOptions = {
-    dataUrl: 'data/titanic_train.csv',
-    inputKeys: ['age', 'fare', 'is_female'],
-    outputKeys: ['survived']
-  };
-  classifier.loadData(dataOptions, dataLoaded);
-
+function setup(){
+  nn = ml5.neuralNetwork(nnOptions, modelReady)
+  
   submitButton = select('#submit');
   submitButton.mousePressed(predict);
   submitButton.hide();
 
 }
 
-function dataLoaded(err, results) {
-  data = results;
-  normalizedData = classifier.normalize(data);
-  console.log(normalizedData);
-  // TODO: epochs could probably be specified here?
-  // TODO: ml5 is auto printing loss to console, should have an option epoch event here?
-  classifier.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
+
+function modelReady(){
+  console.log('classification', nn);
+  nn.data.shuffle();
+  nn.data.normalize();
+
+  const trainingOptions ={
+    epochs: 50,
+    batchSize: 32
+  }
+  nn.train(trainingOptions,finishedTraining);
 }
 
-function finishedTraining(err, results) {
-  console.log('training complete');
+function whileTraining(){
+  console.log('training...')
+}
+
+function finishedTraining(){
+  console.log('done!')
   submitButton.show();
-  select('#result').html('training complete.');
+  classify()
 }
 
-function predict() {
+// TODO: normalize values going into predict! 
+function classify() {
   let age = parseInt(select('#age').value());
   let fare = parseInt(select('#fare').value());
   // TODO: allow for string labels
   let is_female = parseInt(select('#is_female').value());
 
+
   let inputs = [age, fare, is_female];
-  classifier.predict(inputs, function (err, results) {
+
+  nn.classify(inputs, function (err, results) {
     if (err) {
       console.error(err);
     } else {
@@ -67,3 +67,141 @@ function predict() {
     }
   });
 }
+
+
+// /////////////////////////////////////////////
+
+// let classifier;
+// let data;
+// let normalizedData;
+// let submitButton;
+
+// function setup() {
+//   // Create a classification neural network
+
+
+//   // | age   | fareclass | status |
+//   // ------------------------------
+//   // | 50    | "first"   | "lived"  -> 50 0 0 1 * 1 0
+//   // | 25    | "second"  | "died"   -> 25 0 1 0 * 0 1
+//   // | 20    | "third"   | "lived"  -> 20 1 0 0 * 1 0
+
+//   let nnOptions = {
+//     data: 'data/titanic_train.csv',
+//     inputs: ['age', 'fareclass' ],
+//     outputs: ['status'],
+//     task: 'classification'
+//   };
+
+//   ///////////
+//   let nnOptions = {
+//     data: 'data/titanic_train.csv',
+//     inputs: ['age', 'fare', 'is_female'],
+//     outputs: ['survived'],
+//     task: 'classification'
+//     // batchSize: 32,
+//     // epochs: 100
+//   };
+//   // TODO: debug tf.vis off by default?
+//   let classifier = ml5.neuralNetwork(nnOptions);
+
+
+//   // classifier.data.map('fare', val => val / valMax);
+//   classifier.data.normalize();
+//   classifier.data.shuffle();
+
+//   classifier.train(whileTraining, finishedTraining);
+
+
+//   let nnOptions = {
+//     inputs: 2,
+//     outputs: 1
+//   };
+//   // let nnOptions = {
+//   //   inputs: ['mousexpos','mouseypos'],
+//   //   outputs: ['note']
+//   // };
+
+
+//   // TODO: debug tf.vis off by default?
+//   let nn = ml5.neuralNetwork(nnOptions);
+//   classifier.data.add([mouseX, mouseY, value]);
+//   classifier.data.add([mouseX, mouseY], [value]);
+
+
+//   // classifier.data.add({
+//   //   mousexpos: mouseX,
+//   //   mouseypos: mouseY,
+//   //   note: 'A' 
+//   // });
+
+
+//   classifier.train(whileTraining, finishedTraining);
+
+
+
+//   // let nnOptions = {
+//   //   inputs: 3,
+//   //   outputs: 2,
+//   //   task: 'classification',
+//   //   // batchSize: 32,
+//   //   // epochs: 100
+//   // };
+//   // // TODO: debug tf.vis off by default?
+//   // classifier = ml5.neuralNetwork(nnOptions);
+
+//   // // Load data from a CSV into it
+//   // let dataOptions = {
+//   //   dataUrl: 'data/titanic_train.csv',
+//   //   inputKeys: ['age', 'fare', 'is_female'],
+//   //   outputKeys: ['survived']
+//   // };
+//   // classifier.loadData(dataOptions, dataLoaded);
+
+//   submitButton = select('#submit');
+//   submitButton.mousePressed(predict);
+//   submitButton.hide();
+
+// }
+
+// function dataLoaded(err, results) {
+//   data = results;
+//   classifier.shuffle(data)
+//   normalizedData = classifier.normalize(data);
+//   console.log(normalizedData);
+//   // TODO: epochs could probably be specified here?
+//   // TODO: ml5 is auto printing loss to console, should have an option epoch event here?
+//   classifier.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
+// }
+
+// function finishedTraining(err, results) {
+//   console.log('training complete');
+//   submitButton.show();
+//   select('#result').html('training complete.');
+// }
+
+// function predict() {
+//   let age = parseInt(select('#age').value());
+//   let fare = parseInt(select('#fare').value());
+//   // TODO: allow for string labels
+//   let is_female = parseInt(select('#is_female').value());
+
+//   let inputs = [age, fare, is_female];
+//   classifier.predict(inputs, function (err, results) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log(results);
+//       if (results.output[0] > 0.5) {
+//         select('#result').html('prediction: they died');
+//       } else {
+//         select('#result').html('prediction: they lived');
+//       }
+//       // TODO:
+//       // This would only work for classification but maybe we should reformat the output into
+//       // sorted labels and confidence scores?
+//       // console.log(results[0].label);
+//       // console.log(results[0].confidence);
+//     }
+//   });
+// }
