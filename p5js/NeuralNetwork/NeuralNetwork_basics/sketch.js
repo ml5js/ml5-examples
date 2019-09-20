640cfab46bc11f53de7c402d86646285b0d2f3cd
joeyklee <jsphknglee@gmail.com>
2019-09-19 21:08:05 -0400

    updates basic -- broken

--- p5js/NeuralNetwork/NeuralNetwork_basics/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_basics/sketch.js
@@ -8,52 +8,54 @@ ml5 Example
 Image classification using MobileNet and p5.js
 This example uses a callback pattern to create the classifier
 === */
-let neuralNetwork;
+let nn;
 
 function setup() {
   // Options for Neural Network
   const options = {
-    input: 3,
-    output: 2,
-    activation: 'tanh'
+    inputs: 3,
+    outputs: 2,
+    task:'regression',
+    activationOutput: 'sigmoid',
+    activationHidden: 'sigmoid'
   };
   // Create Neural Network
-  neuralNetwork = ml5.neuralNetwork(options);
+  nn = ml5.neuralNetwork(options);
 
   // Train the model
   let trainBtn = createButton('Train Model');
   trainBtn.position(10, 50);
-  trainBtn.mousePressed(function() {
+  trainBtn.mousePressed(function () {
     trainModel();
   });
 
   // Predict
   let predictBtn = createButton('Predict');
   predictBtn.position(10, 70);
-  predictBtn.mousePressed(function() {
+  predictBtn.mousePressed(function () {
     predict();
   });
 
   // Save and download the model
   let saveBtn = createButton('Save Model');
   saveBtn.position(10, 90);
-  saveBtn.mousePressed(function() {
+  saveBtn.mousePressed(function () {
     neuralNetwork.save();
   });
 
   // Load the model from local files
   let loadLocalBtn = createButton('Load the model from local files');
   loadLocalBtn.position(10, 110);
-  loadLocalBtn.mousePressed(function() {
-    neuralNetwork.load('model/model.json', function() {
+  loadLocalBtn.mousePressed(function () {
+    neuralNetwork.load('model/model.json', function () {
       console.log('Model Loaded!');
     });
   });
 
   // Load model
   let loadBtn = select('#load');
-  loadBtn.changed(function() {
-    neuralNetwork.load(loadBtn.elt.files, function() {
+  loadBtn.changed(function () {
+    neuralNetwork.load(loadBtn.elt.files, function () {
       console.log('Model Loaded!');
     });
   });
@@ -63,29 +65,38 @@ function trainModel() {
   // Add training data
   const training_input = [-0.6, 1, 0.25];
   const training_target = [0.3, 0.9];
+
   for (let i = 0; i < 500; i++) {
-    neuralNetwork.addData(training_input, training_target);
+    nn.data.addData({
+      input0: training_input[0],
+      input1: training_input[1],
+      input2: training_input[2],
+    }, 
+    {
+      output0: training_target[0],
+      output1: training_target[1],
+    });
   }
 
+  const trainingOptions = {
+    epochs: 10,
+    batchSize: 32
+  }
   // Train
-  neuralNetwork.train(20, whileTraining);
+  nn.data.shuffle();
+  nn.data.normalize();
+  nn.train(trainingOptions, finishedTraining);
 }
 
 // Training callback
-function whileTraining(error, progress) {
-  if (progress.status == 'training') {
-    console.log(progress.epoch, progress.loss);
-  } else if (progress.status == 'complete') {
-    // Run prediction when complete
-    predict();
-  }
+function finishedTraining() {
+  predict();
 }
 
 function predict() {
   let input = [-0.6, 1, 0.25];
-  neuralNetwork.predict(input, gotResults);
-  // const output = await neuralNetwork.predict(input);
-  // console.log('output: ', output);
+  nn.predict(input, gotResults);
+
 }
 
 function gotResults(error, results) {
