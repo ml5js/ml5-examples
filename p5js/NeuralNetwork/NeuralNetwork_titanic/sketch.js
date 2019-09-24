b7e62b46b47f488fe08446b502b097e78658cc60
shiffman <daniel@shiffman.net>
2019-09-24 11:35:35 -0400

    training is working but issue with predict and passing in strings

--- p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_titanic/sketch.js
@@ -49,8 +49,15 @@ function classify() {
   let fare_class = select('#fare_class').value();
   let sex = select('#sex').value();
 
-  let inputs = { age, fare, fare_class, sex };
+  // TODO: This might be a convenient way to pass in values to predict?
+  // let inputs = { age, fare, fare_class, sex };
+  // neuralNetwork.classify(inputs, gotResults);
+
+  // inputs: ['fare_class', 'sex', 'age', 'fare'],
+  let inputs = [fare_class, sex, age, fare,];
   neuralNetwork.classify(inputs, gotResults);
+
+
 }
 
 function gotResults(err, results) {
@@ -70,140 +77,3 @@ function gotResults(err, results) {
     // console.log(results[0].confidence);
   }
 }
-
-// /////////////////////////////////////////////
-
-// let classifier;
-// let data;
-// let normalizedData;
-// let submitButton;
-
-// function setup() {
-//   // Create a classification neural network
-
-
-//   // | age   | fareclass | status |
-//   // ------------------------------
-//   // | 50    | "first"   | "lived"  -> 50 0 0 1 * 1 0
-//   // | 25    | "second"  | "died"   -> 25 0 1 0 * 0 1
-//   // | 20    | "third"   | "lived"  -> 20 1 0 0 * 1 0
-
-//   let nnOptions = {
-//     data: 'data/titanic_train.csv',
-//     inputs: ['age', 'fareclass' ],
-//     outputs: ['status'],
-//     task: 'classification'
-//   };
-
-//   ///////////
-//   let nnOptions = {
-//     data: 'data/titanic_train.csv',
-//     inputs: ['age', 'fare', 'is_female'],
-//     outputs: ['survived'],
-//     task: 'classification'
-//     // batchSize: 32,
-//     // epochs: 100
-//   };
-//   // TODO: debug tf.vis off by default?
-//   let classifier = ml5.neuralNetwork(nnOptions);
-
-
-//   // classifier.data.map('fare', val => val / valMax);
-//   classifier.data.normalize();
-//   classifier.data.shuffle();
-
-//   classifier.train(whileTraining, finishedTraining);
-
-
-//   let nnOptions = {
-//     inputs: 2,
-//     outputs: 1
-//   };
-//   // let nnOptions = {
-//   //   inputs: ['mousexpos','mouseypos'],
-//   //   outputs: ['note']
-//   // };
-
-
-//   // TODO: debug tf.vis off by default?
-//   let nn = ml5.neuralNetwork(nnOptions);
-//   classifier.data.add([mouseX, mouseY, value]);
-//   classifier.data.add([mouseX, mouseY], [value]);
-
-
-//   // classifier.data.add({
-//   //   mousexpos: mouseX,
-//   //   mouseypos: mouseY,
-//   //   note: 'A'
-//   // });
-
-
-//   classifier.train(whileTraining, finishedTraining);
-
-
-
-//   // let nnOptions = {
-//   //   inputs: 3,
-//   //   outputs: 2,
-//   //   task: 'classification',
-//   //   // batchSize: 32,
-//   //   // epochs: 100
-//   // };
-//   // // TODO: debug tf.vis off by default?
-//   // classifier = ml5.neuralNetwork(nnOptions);
-
-//   // // Load data from a CSV into it
-//   // let dataOptions = {
-//   //   dataUrl: 'data/titanic_train.csv',
-//   //   inputKeys: ['age', 'fare', 'is_female'],
-//   //   outputKeys: ['survived']
-//   // };
-//   // classifier.loadData(dataOptions, dataLoaded);
-
-//   submitButton = select('#submit');
-//   submitButton.mousePressed(predict);
-//   submitButton.hide();
-
-// }
-
-// function dataLoaded(err, results) {
-//   data = results;
-//   classifier.shuffle(data)
-//   normalizedData = classifier.normalize(data);
-//   console.log(normalizedData);
-//   // TODO: epochs could probably be specified here?
-//   // TODO: ml5 is auto printing loss to console, should have an option epoch event here?
-//   classifier.train(normalizedData.inputs, normalizedData.targets, finishedTraining);
-// }
-
-// function finishedTraining(err, results) {
-//   console.log('training complete');
-//   submitButton.show();
-//   select('#result').html('training complete.');
-// }
-
-// function predict() {
-//   let age = parseInt(select('#age').value());
-//   let fare = parseInt(select('#fare').value());
-//   // TODO: allow for string labels
-//   let is_female = parseInt(select('#is_female').value());
-
-//   let inputs = [age, fare, is_female];
-//   classifier.predict(inputs, function (err, results) {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       console.log(results);
-//       if (results.output[0] > 0.5) {
-//         select('#result').html('prediction: they died');
-//       } else {
-//         select('#result').html('prediction: they lived');
-//       }
-//       // TODO:
-//       // This would only work for classification but maybe we should reformat the output into
-//       // sorted labels and confidence scores?
-//       // console.log(results[0].label);
-//       // console.log(results[0].confidence);
-//     }
-//   });
-// }
