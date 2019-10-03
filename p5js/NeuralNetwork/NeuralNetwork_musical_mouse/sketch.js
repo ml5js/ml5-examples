960986d8fc8530f53c39dca6973d51dce0da4405
shiffman <daniel@shiffman.net>
2019-10-02 20:39:32 -0400

    code comments

--- p5js/NeuralNetwork/NeuralNetwork_musical_mouse/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_musical_mouse/sketch.js
@@ -1,61 +1,88 @@
-let notePlayer;
+// The neural network is the brain
+let brain;
+
+// Variables to track p5 oscillator
 let playing = false;
 let frequency;
 let osc;
 
 function setup() {
-  createCanvas(400, 400).mousePressed(addData);;
+  let canvas = createCanvas(400, 400);
+  // Only when clicking on the canvas
+  canvas.mousePressed(addData);
+
+  // Create ethe model
   const options = {
     inputs: 2,  // TODO: support ['x', 'y']
     outputs: 1, // TODO: support ['freq]
     debug: true,
   }
-  background(0);
-  notePlayer = ml5.neuralNetwork(options);
+  brain = ml5.neuralNetwork(options);
+
+  // Train Model button
   select('#train').mousePressed(trainModel);
+
+  background(0);
 }
 
+// Add a data record
 function addData() {
+  // Get frequency
+  let target = parseFloat(select('#frequency').value());
+  // TODO: support notePlayer.data.addData({x: mouseX, y: mouseY}, {frequency: target});
+  // Add data
+  brain.addData([mouseX, mouseY], [target]);
 
-  let freq = select('#frequency').value();
+  // Draw circle to visualize training data
   stroke(255);
   noFill();
   ellipse(mouseX, mouseY, 32);
   fill(255);
   textSize(16);
-  console.log(freq);
   textAlign(CENTER, CENTER);
-  text(freq, mouseX, mouseY);
-  // TODO: support notePlayer.data.addData({x: mouseX, y: mouseY}, [parseFloat(freq)]);
-  notePlayer.data.addData([mouseX, mouseY], [parseFloat(freq)]);
+  text(target, mouseX, mouseY);
 }
 
+// Train the model
 function trainModel() {
-  notePlayer.normalizeData();
-  const trainingOptions = {
-    epochs: 20
-  }
-  notePlayer.train(trainingOptions, finishedTraining);
+  // ml5 will normalize data to a range between 0 and 1 for you.
+  brain.normalizeData();
+  // Train the model
+  // Epochs: one cycle through all the training data
+  brain.train({ epochs: 20 }, finishedTraining);
 }
 
+// When the model is trained
 function finishedTraining() {
-  console.log('done');
+  // Start playing sound
   osc = new p5.Oscillator();
   osc.setType('sine');
   osc.amp(0.5);
   osc.freq(440);
   osc.start();
-  notePlayer.predict([mouseX, mouseY], gotFrequency);
+
+  // Predict a frequency
+  brain.predict([mouseX, mouseY], gotFrequency);
 }
 
+// Got a result
 function gotFrequency(error, outputs) {
   if (error) {
     console.error(error);
     return;
   }
+
+  // The output comes in an array
+  // There is only one output in this example, but it could be more!
   frequency = parseFloat(outputs[0].value);
+
+  // Render frequency value
   select('#prediction').html(frequency.toFixed(2));
+
+  // Adjust oscillator
   osc.freq(parseFloat(frequency));
-  notePlayer.predict([mouseX, mouseY], gotFrequency);
+
+  // Predict again
+  brain.predict([mouseX, mouseY], gotFrequency);
 }
 
