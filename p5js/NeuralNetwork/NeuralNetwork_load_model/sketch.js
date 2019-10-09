957cda98770706f7709ce6d3e116071eebefc48b
joeyklee <jsphknglee@gmail.com>
2019-10-09 16:16:47 -0400

    updates example showcasing issues

--- p5js/NeuralNetwork/NeuralNetwork_load_model/sketch.js
+++ p5js/NeuralNetwork/NeuralNetwork_load_model/sketch.js
@@ -21,14 +21,51 @@ function setup() {
     inputs: ['r', 'g', 'b'],
     outputs: ['label'],
     task: 'classification',
+    normalizationOptions: {
+      inputMax: [255, 255, 255],
+      inputMin: [0, 0, 0],
+      outputMax: [1, 1, 1, 1, 1, 1, 1, 1, 1],
+      outputMin: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    },
     debug: true
   };
   neuralNetwork = ml5.neuralNetwork(nnOptions);
   neuralNetwork.load('model/model.json', modelReady);
 }
 
-function modelReady(){
+function modelReady() {
   console.log('model loaded!')
+  neuralNetwork.data.meta.inputs = {
+    r: {
+      dtype: 'number'
+    },
+    g: {
+      dtype: 'number'
+    },
+    b: {
+      dtype: 'number'
+    }
+  }
+  neuralNetwork.data.meta.outputUnits = 9;
+  neuralNetwork.data.meta.outputs = {
+    label: {
+      dtype: 'string',
+      uniqueValues: ["green-ish", "pink-ish", "orange-ish", "blue-ish", "brown-ish", "red-ish", "yellow-ish", "purple-ish", "grey-ish"],
+      legend: {
+        "green-ish": [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        "pink-ish": [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        "orange-ish": [0, 0, 1, 0, 0, 0, 0, 0, 0],
+        "blue-ish": [0, 0, 0, 1, 0, 0, 0, 0, 0],
+        "brown-ish": [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        "red-ish": [0, 0, 0, 0, 0, 1, 0, 0, 0],
+        "yellow-ish": [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        "purple-ish": [0, 0, 0, 0, 0, 0, 0, 1, 0],
+        "grey-ish": [0, 0, 0, 0, 0, 0, 0, 0, 1]
+      }
+    },
+
+  }
+
   classify();
 };
 
@@ -39,7 +76,7 @@ function classify() {
     g: gSlider.value(),
     b: bSlider.value()
   }
-  neuralNetwork.classify([inputs.r, inputs.g, inputs.b], gotResults);
+  neuralNetwork.classify({r:inputs.r, g:inputs.g, b:inputs.b}, gotResults);
 }
 
 function gotResults(error, results) {
