4e3ae6a5127dde4dacc6627f140b7efd7fce7307
joeyklee <jsphknglee@gmail.com>
2019-11-25 18:37:33 -0500

    updates predict function

--- p5js/DiyNeuralNetwork/DiyNeuralNetwork_classification_color/sketch.js
+++ p5js/DiyNeuralNetwork/DiyNeuralNetwork_classification_color/sketch.js
@@ -13,108 +13,133 @@ let inputs, outputs;
 
 // function setup(){
 
-  nn = ml5.diyNeuralNetwork();
-  
-  nn.neuralNetworkData.loadJSON('colorData.json', ['r', 'g', 'b'], ['label']).then( () =>{
-    
-    const meta = nn.neuralNetworkData.createMetaDataFromData(nn.neuralNetworkData.data.raw)
-    
-    nn.neuralNetworkData.meta = meta;
-    // create a model
-    nn.neuralNetwork.createModel('sequential');
-    
-    // get the inputUnits and outputUnits
-    const {inputUnits, outputUnits} = nn.neuralNetworkData.meta
-    // add some layers
-    nn.neuralNetwork.addLayer(nn.createDenseLayer({inputShape: [inputUnits]}))
-    nn.neuralNetwork.addLayer(nn.createDenseLayer({activation: 'sigmoid'}))
-    nn.neuralNetwork.addLayer(nn.createDenseLayer({units: outputUnits, activation: 'sigmoid'}))
-    // compile the model
-    nn.compile({
-      loss: 'meanSquaredError',
-      optimizer: ml5.tf.train.adam, 
-      metrics: ['accuracy'],
-    });
+nn = ml5.diyNeuralNetwork();
+
+nn.neuralNetworkData.loadJSON('colorData.json', ['r', 'g', 'b'], ['label']).then(() => {
+
+  const meta = nn.neuralNetworkData.createMetaDataFromData(nn.neuralNetworkData.data.raw)
+
+  nn.neuralNetworkData.meta = meta;
+  // create a model
+  nn.neuralNetwork.createModel('sequential');
+
+  // get the inputUnits and outputUnits
+  const {
+    inputUnits,
+    outputUnits
+  } = nn.neuralNetworkData.meta
+  // add some layers
+  nn.neuralNetwork.addLayer(nn.createDenseLayer({
+    inputShape: [inputUnits]
+  }))
+  nn.neuralNetwork.addLayer(nn.createDenseLayer({
+    activation: 'sigmoid'
+  }))
+  nn.neuralNetwork.addLayer(nn.createDenseLayer({
+    units: outputUnits,
+    activation: 'sigmoid'
+  }))
+  // compile the model
+  nn.compile({
+    loss: 'meanSquaredError',
+    optimizer: ml5.tf.train.adam,
+    metrics: ['accuracy'],
+  });
 
 
-    // let inputMeta; 
-    nn.warmUp();
-    // nn.normalizeData();
-    // console.log(trainingData)
+  // let inputMeta; 
+  nn.warmUp();
+  nn.normalizeData();
+
+  console.log(ml5.tf.memory())
+
+  nn.train({
+    epochs: 3,
+    batchSize: 150,
+    validationSplit: 0.1,
+    whileTraining: function (epoch, loss) {
+      console.log(epoch, loss.loss)
+    }
+  }, finishedTraining)
+
+
+
+  function finishedTraining() {
+
     console.log(ml5.tf.memory())
-    nn.train({
-      epochs: 10,
-      batchSize: 150,
-      validationSplit: 0.1,
-      whileTraining: function(epoch, loss){
-        console.log(epoch, loss.loss)
-      }
-    }, function(){
-      console.log(ml5.tf.memory())
-      console.log(nn, 'training done!')
+    console.log(nn, 'training done!')
 
-      
-      const testInput = ml5.tf.tensor([[1, 0,0]]);
-      
-      nn.neuralNetwork.predict(testInput, function(err, result){
-        console.log(result)
-        
+    nn.predict({
+      r: 1,
+      g: 0,
+      b: 0
+    }, gotResult)
 
-        const vals = Object.entries(nn.neuralNetworkData.meta.outputs.label.legend);
-        vals.forEach( (item,idx) => {
-          console.log(`label:${item[0]}, confidence:${result[0][idx]}`);
-        })
-        
-      })
+    nn.predict([1, 0, 0], gotResult)
 
-      testInput.dispose();
+  }
+
+  function gotResult(err, result) {
+    if (err) {
+      console.log('err', err);
+      return
+    }
+    console.log(result)
+
+    const vals = Object.entries(nn.neuralNetworkData.meta.outputs.label.legend);
+    vals.forEach((item, idx) => {
+      console.log(`label:${item[0]}, confidence:${result[0][idx]}`);
     })
 
 
-  })
+    console.log(ml5.tf.memory())
+  }
 
-  
+
+})
 
 
-    // const inputArr = [];
-    // const outputArr = [];
-    // nn.neuralNetworkData.data.raw.forEach( row => {
-    //   // get xs
-    //   // const xs = ['r', 'g', 'b'].map(k => row.xs[k]);
-    //   const xs = Object.keys(nn.neuralNetworkData.meta.inputs).map(k => {
-    //     if(nn.neuralNetworkData.meta.inputs[k].legend){
-    //       return nn.neuralNetworkData.meta.inputs[k].legend[row.xs[k]]
-    //     }
-    //     return row.xs[k]
-    //   });
-    //   inputArr.push(...xs)
 
-    //   // get ys
-    //   const ys = Object.keys(nn.neuralNetworkData.meta.outputs).map(k =>  {
-    //     if(nn.neuralNetworkData.meta.outputs[k].legend){
-    //       return nn.neuralNetworkData.meta.outputs[k].legend[row.ys[k]]
-    //     }
-    //     return row.ys[k]
-    //   })
-    //   outputArr.push(...ys)
 
-    // })
-    // const inputs = ml5.tf.tensor(inputArr, [nn.neuralNetworkData.data.raw.length, inputUnits])
-    // const outputs = ml5.tf.tensor(outputArr, [nn.neuralNetworkData.data.raw.length, outputUnits])
+// const inputArr = [];
+// const outputArr = [];
+// nn.neuralNetworkData.data.raw.forEach( row => {
+//   // get xs
+//   // const xs = ['r', 'g', 'b'].map(k => row.xs[k]);
+//   const xs = Object.keys(nn.neuralNetworkData.meta.inputs).map(k => {
+//     if(nn.neuralNetworkData.meta.inputs[k].legend){
+//       return nn.neuralNetworkData.meta.inputs[k].legend[row.xs[k]]
+//     }
+//     return row.xs[k]
+//   });
+//   inputArr.push(...xs)
 
-  
-  // train the model
-  // const training_options = {
-  //   inputs,
-  //   outputs,
-  //   batchSize: 32,
-  //   epochs: 10,
-  //   whileTraining: (epoch, loss) => {
-  //     console.log(`epoch:${epoch}, loss: ${loss.loss}`);
-  //   }
-  // }
-  // nn.train(training_options, finishedTraining)
-  
+//   // get ys
+//   const ys = Object.keys(nn.neuralNetworkData.meta.outputs).map(k =>  {
+//     if(nn.neuralNetworkData.meta.outputs[k].legend){
+//       return nn.neuralNetworkData.meta.outputs[k].legend[row.ys[k]]
+//     }
+//     return row.ys[k]
+//   })
+//   outputArr.push(...ys)
+
+// })
+// const inputs = ml5.tf.tensor(inputArr, [nn.neuralNetworkData.data.raw.length, inputUnits])
+// const outputs = ml5.tf.tensor(outputArr, [nn.neuralNetworkData.data.raw.length, outputUnits])
+
+
+// train the model
+// const training_options = {
+//   inputs,
+//   outputs,
+//   batchSize: 32,
+//   epochs: 10,
+//   whileTraining: (epoch, loss) => {
+//     console.log(`epoch:${epoch}, loss: ${loss.loss}`);
+//   }
+// }
+// nn.train(training_options, finishedTraining)
+
 // }
 
 // function finishedTraining(){
@@ -125,7 +150,7 @@ let inputs, outputs;
 
 //     console.log(nn)
 //     const testInput = ml5.tf.tensor([[0], [0.25], [0.5], [0.75], [1]])
-    
+
 //     nn.neuralNetwork.predict(testInput, function(err, result){
 //       console.log('hi from callback', result)
 //     })
