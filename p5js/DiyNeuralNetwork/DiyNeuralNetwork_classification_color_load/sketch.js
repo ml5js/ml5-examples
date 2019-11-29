d203b2fb88df6a319d7d86ce864af6168ac25744
joeyklee <jsphknglee@gmail.com>
2019-11-29 11:10:39 -0500

    updates load example

--- p5js/DiyNeuralNetwork/DiyNeuralNetwork_classification_color_load/sketch.js
+++ p5js/DiyNeuralNetwork/DiyNeuralNetwork_classification_color_load/sketch.js
@@ -9,39 +9,38 @@ Image classification using MobileNet and p5.js
 This example uses a callback pattern to create the classifier
 === */
 let nn;
-// function setup(){
 
-const options = {
-  // inputs: ['r', 'g', 'b'],
-  // outputs: ['label'],
-  // dataUrl: 'colorData.json',
-  task: 'classification'
-}
+function setup() {
 
-nn = ml5.diyNeuralNetwork(options);
-
-const modelUrls = {
-  model: 'model/model.json',
-  metadata: 'model/model_meta.json',
-  weights: 'model/model.weights.bin'
-}
-nn.load(modelUrls, modelLoaded)
-
-function modelLoaded() {
-
-  nn.classify({
-    r: 1,
-    g: 0,
-    b: 0
-  }, gotResult)
-
-}
-
-function gotResult(err, result) {
-  if (err) {
-    console.log('err', err);
-    return
+  const options = {
+    task: 'classification'
   }
-  console.log(result)
+
+  nn = ml5.diyNeuralNetwork(options);
+
+  const modelUrls = {
+    model: 'model/model.json',
+    metadata: 'model/model_meta.json',
+    weights: 'model/model.weights.bin'
+  }
+  nn.load(modelUrls, modelLoaded)
+
+  function modelLoaded() {
+
+    nn.classify({
+      r: 255,
+      g: 0,
+      b: 0
+    }, gotResult)
+
+  }
+
+  function gotResult(err, result) {
+    if (err) {
+      console.log('err', err);
+      return
+    }
+    console.log(result)
+  }
 
 }
