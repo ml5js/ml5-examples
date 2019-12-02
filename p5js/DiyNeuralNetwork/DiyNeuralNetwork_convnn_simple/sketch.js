5c42aad208c7899fc950a8d5946efe8e048d7735
joeyklee <jsphknglee@gmail.com>
2019-12-02 18:35:20 -0500

    adds example of conv neural net with custom layers

--- p5js/DiyNeuralNetwork/DiyNeuralNetwork_convnn_simple/sketch.js
+++ p5js/DiyNeuralNetwork/DiyNeuralNetwork_convnn_simple/sketch.js
@@ -15,164 +15,117 @@ const IMAGE_CHANNELS = 4;
 
 let images;
 let testA;
-function preload(){
+
+function preload() {
   images = [];
-  for(let i = 1; i < 7; i++){
+  for (let i = 1; i < 7; i++) {
     const a = loadImage(`images/A_0${i}.png`)
     const b = loadImage(`images/B_0${i}.png`)
-    images.push({image:a, label: 'a'})
-    images.push({image:b, label: 'b'})
+    images.push({
+      image: a,
+      label: 'a'
+    })
+    images.push({
+      image: b,
+      label: 'b'
+    })
   }
 
   testA = loadImage(`images/A_test.png`)
-  
+
 }
 
 function setup() {
-    images.forEach(item => item.image.loadPixels())
-    console.log(images)
-    const options = {
-      task: 'classification',
-      layers: [
-        {
-          type:'conv2d',
-          inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-          kernelSize: 5,
-          filters: 8,
-          strides: 1,
-          activation: 'relu',
-          kernelInitializer: 'varianceScaling'
-        },
-        {
-          type:'maxPooling2d',
-          poolSize: [2, 2], strides: [2, 2]
-        },
-        {
-          type:'conv2d',
-          filters: 16,
-          activation: 'relu',
-          kernelInitializer: 'varianceScaling'
-        },
-        {
-          type:'maxPooling2d',
-          poolSize: [2, 2], strides: [2, 2]
-        },
-        {
-          type:'flatten'
-        },
-        {
-          type:'dense',
-          units:2,
-          kernelInitializer: 'varianceScaling',
-          activation: 'softmax'
-        }
-      ]
-    }
+  // load the pixels for each image to get a flat pixel array
+  images.forEach(item => item.image.loadPixels())
 
-    nn = ml5.neuralNetwork(options);
-
-    const BATCH_SIZE = 2;
-    // const TRAIN_DATA_SIZE = 5500;
-    // const TEST_DATA_SIZE = 1000;
-
-    const inputs = ml5.tf.tidy( () => {
-      const imageList = images.map(item => {
-        const arr = Array.from(item.image.pixels)
-        const min = nn.neuralNetworkData.getMin(arr);
-        const max = nn.neuralNetworkData.getMax(arr);
-
-        return nn.neuralNetworkData.normalizeArray(arr, {min, max})
-      })
-      return ml5.tf.tensor(imageList, [images.length, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS])
-    })
-    
-    const oneHotLabels = nn.neuralNetworkData.createOneHotEncodings(['a', 'b'])
-    const labelList = images.map(item => {
-      return oneHotLabels.legend[item.label]
-    })
-    
-    const outputs = ml5.tf.tensor(labelList, [images.length, oneHotLabels.uniqueValues.length])
-
-    // const [trainXs, trainYs] = ml5.tf.tidy(() => {
-    //   const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
-    //   return [
-    //     d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
-    //     d.labels
-    //   ];
-    // });
-  
-    // const [testXs, testYs] = ml5.tf.tidy(() => {
-    //   const d = data.nextTestBatch(TEST_DATA_SIZE);
-    //   return [
-    //     d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
-    //     d.labels
-    //   ];
-    // });
-  
-    // compile the model
-    // nn.compile({
-    //   loss: 'categoricalCrossentropy',
-    //   metrics: ['accuracy']}
-    // );
-
-    nn.train({
-      inputs,
-      outputs,
-      batchSize: BATCH_SIZE,
-      // validationData: [inputs, outputs],
-      epochs: 32,
-      shuffle: false,
-      whileTraining: (epoch, loss) => {
-        console.log(`epoch:${epoch}, loss: ${loss.loss}`);
+  const options = {
+    task: 'classification',
+    debug: true,
+    layers: [{
+        type: 'conv2d',
+        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS], // for image classifcation this is necessary 
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
       },
-    }, finishedTraining)
-
-
-  function finishedTraining(err, result){
-
-    if(err) {
-      console.log(err); 
-      return
-    }
-
-    testA.loadPixels();
-    const normalized = nn.neuralNetworkData.normalizeArray( Array.from(testA.pixels),{min: 0, max:255});
-    testATensor = ml5.tf.tensor( [normalized ], [1, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS])
-    
-    nn.neuralNetwork.classify(testATensor, {outputs:{label:oneHotLabels}}, (err, result) =>{
-      if(err){
-        console.log(err);
-        return;
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2]
+      },
+      {
+        type: 'conv2d',
+        filters: 16,
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+      },
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2]
+      },
+      {
+        type: 'flatten'
+      },
+      {
+        type: 'dense',
+        // units:2, // will be handled by the neural net class
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
       }
-      console.log(result);
-      testATensor.dispose();
-    })
-
-    inputs.dispose();
-    outputs.dispose();
-    // console.log(doPrediction(nn.neuralNetwork.model, data))
+    ]
   }
 
+  // construct the neural network
+  nn = ml5.neuralNetwork(options);
+
+  // add data
+  images.forEach(item => {
+    const imageArray = Array.from(item.image.pixels);
+    const labels = item.label;
+    nn.neuralNetworkData.addImageData(imageArray, {
+      label: labels
+    });
+  })
+
+  nn.normalizeData();
+
+  const TRAINING_OPTIONS = {
+    batchSize: 2,
+    onImageData: true,
+    // validationData: [inputs, outputs],
+    epochs: 10,
+    shuffle: false,
+  }
+
+  nn.train(TRAINING_OPTIONS, finishedTraining)
 
 }
 
 
+function finishedTraining(err, result) {
 
+  if (err) {
+    console.log(err);
+    return
+  }
 
+  console.log("finished training");
 
-// function doPrediction(model, data, testDataSize = 500) {
-//   const IMAGE_WIDTH = 28;
-//   const IMAGE_HEIGHT = 28;
-//   const testData = data.nextTestBatch(testDataSize);
-//   const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
-//   const labels = testData.labels.argMax([-1]);
-//   const preds = model.predict(testxs).argMax([-1]);
-//   const output = {
-//     labels: labels.arraySync(),
-//     prediction: preds.arraySync()
-//   }
-//   testxs.dispose();
-//   preds.dispose();
-//   labels.dispose();
-//   return output
-// }
+  nn.classifyImage(testA.pixels, gotResult)
+
+}
+
+function gotResult(err, result) {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log(result);
+}
