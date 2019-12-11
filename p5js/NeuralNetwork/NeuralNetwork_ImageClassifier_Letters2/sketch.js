9e1272a4ca4e82fbd490888959d25ed4ea0eef5b
joeyklee <jsphknglee@gmail.com>
2019-12-11 16:08:51 -0500

    adds updates to image classificaiton example

---
+++ p5js/NeuralNetwork/NeuralNetwork_ImageClassifier_Letters2/sketch.js
@@ -0,0 +1,117 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Image classification using MobileNet and p5.js
+This example uses a callback pattern to create the classifier
+=== */
+let nn;
+const IMAGE_WIDTH = 64;
+const IMAGE_HEIGHT = 64;
+const IMAGE_CHANNELS = 4;
+
+let images;
+let testA;
+
+function preload() {
+  images = [];
+  for (let i = 1; i < 7; i++) {
+    const a = loadImage(`images/A_0${i}.png`)
+    const b = loadImage(`images/B_0${i}.png`)
+    images.push({
+      image: a,
+      label: 'a'
+    })
+    images.push({
+      image: b,
+      label: 'b'
+    })
+  }
+
+  testA = loadImage(`images/A_test.png`)
+
+}
+
+function setup() {
+  // load the pixels for each image to get a flat pixel array  
+
+  const options = {
+    task: 'imageClassification',
+    debug: true,
+    inputs:[IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+    layers: [{
+        type: 'conv2d',
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+      },
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2]
+      },
+      {
+        type: 'conv2d',
+        filters: 16,
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+      },
+      {
+        type: 'maxPooling2d',
+        poolSize: [2, 2],
+        strides: [2, 2]
+      },
+      {
+        type: 'flatten'
+      },
+      {
+        type: 'dense',
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
+      }
+    ]
+  }
+
+  // construct the neural network
+  nn = ml5.neuralNetwork(options);
+
+  // add data
+  images.forEach(item => {
+    const labels = item.label;
+    nn.addData({pixelArray:item.image}, {label: labels});
+  })
+
+  // nn.normalizeData();
+
+  const TRAINING_OPTIONS = {
+    batchSize: 2,
+    epochs: 10,
+  }
+
+  nn.train(TRAINING_OPTIONS, finishedTraining)
+
+}
+
+
+function finishedTraining() {
+
+  console.log("finished training");
+  nn.classify({pixelArray:testA}, gotResults)
+
+}
+
+function gotResults(err, result) {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log(result);
+}
