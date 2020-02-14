d3e478f84899fc1520d486189265c5c039ec41d0
joeyklee <jsphknglee@gmail.com>
2020-02-14 13:05:59 -0500

    adds cartoon gan example

---
+++ p5js/CartoonGan/CartoonGan_Basic/sketch.js
@@ -0,0 +1,25 @@
+let cartoonGan;
+let img;
+function preload(){
+    img = loadImage('MrBubz.jpg');
+}
+
+function setup(){
+    createCanvas(400, 400);
+
+    cartoonGan = ml5.cartoon(modelLoaded)
+    image(img, 0,0, width, height)
+}
+
+function modelLoaded(){
+    cartoonGan.generate(img, gotResults)
+}
+
+function gotResults(err, result){
+    if(err){
+        return;
+    }
+    console.log(result);
+    image(result.image, 0,0, 256, 256);
+}
+
